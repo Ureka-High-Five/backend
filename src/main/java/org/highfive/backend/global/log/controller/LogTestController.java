3f760c32c6dfe5b0e18006e8fecd772593125e03
sangyunpark99 <psangyun5108@gmail.com>
2025-07-04 16:07:43 +0900

    feat[#1]: 로그 테스트시 사용하는 LogTestController 구현

--- src/main/java/org/highfive/backend/global/log/controller/LogTestController.java
+++ src/main/java/org/highfive/backend/global/log/controller/LogTestController.java
@@ -1,15 +1,17 @@
-package org.highfive.backend;
+package org.highfive.backend.global.log.controller;
 
+import jakarta.validation.constraints.Max;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RestController
 public class LogTestController {
-
     @GetMapping("/log")
-    public String logTest() {
+    public String logTest(@Validated @Max(3) @RequestParam String message) {
         log.info("CloudWatch 연동 테스트 - 로그 출력됨!");
         return "로그 테스트 완료";
     }
