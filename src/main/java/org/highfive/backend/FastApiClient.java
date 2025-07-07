d0cef3d775baf0592abc1073aad2f32a499ad826
likerhythm <wjdalsdl0412@gmail.com>
2025-07-07 23:02:27 +0900

    feat: 온보딩 처리 요청 구현

---
+++ src/main/java/org/highfive/backend/FastApiClient.java
@@ -0,0 +1,40 @@
+package org.highfive.backend;
+
+import java.util.List;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+@Component
+public class FastApiClient {
+
+    private final String fastApiUrl = "http://localhost:8000";
+    private final RestTemplate restTemplate = new RestTemplate();
+
+    /**
+     * 온보딩 화면에서 선택한 컨텐츠를 FastAPI 서버에 전달합니다.
+     * FastAPI 서버는 사용자의 초기 가중치와 벡터를 저장합니다.
+     *
+     * @param contentIds // 온보딩 화면에서 선택한 컨텐츠 id
+     * @return // 가중치와 벡터 저장 성공 시 true 아니면 false
+     */
+    public Boolean onboarding(final List<Integer> contentIds) {
+        String url = genUrl("/onboarding");
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        HttpEntity<List<Integer>> request = new HttpEntity<>(contentIds, headers);
+
+        ResponseEntity<Boolean> response = restTemplate.postForEntity(url, request, Boolean.class);
+
+        return response.getBody();
+    }
+    private String genUrl(String endPoint) {
+        return fastApiUrl + endPoint;
+    }
+}
