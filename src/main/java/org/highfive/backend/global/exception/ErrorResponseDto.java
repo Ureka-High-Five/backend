55aaf59a34f4e51443cfc9cac719b0309cd9cc36
sangyunpark99 <psangyun5108@gmail.com>
2025-07-08 03:22:00 +0900

    refactor[#41]: code 필드 int 형으로 변환

--- src/main/java/org/highfive/backend/global/exception/ErrorResponseDto.java
+++ src/main/java/org/highfive/backend/global/exception/ErrorResponseDto.java
@@ -2,7 +2,7 @@ package org.highfive.backend.global.exception;
 
 import org.highfive.backend.global.code.ErrorCode;
 
-public record ErrorResponseDto(String code, String message) {
+public record ErrorResponseDto(int code, String message) {
     public ErrorResponseDto(final ErrorCode errorCode) {
         this(errorCode.getCode(), errorCode.getMessage());
     }
