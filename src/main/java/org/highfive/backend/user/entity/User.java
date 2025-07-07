1a005eaa0d487d2a5b7940d81add94ffe878a8b1
likerhythm <wjdalsdl0412@gmail.com>
2025-07-08 02:14:01 +0900

    chore[#34]: User 엔티티 Setter 추가

--- src/main/java/org/highfive/backend/user/entity/User.java
+++ src/main/java/org/highfive/backend/user/entity/User.java
@@ -5,6 +5,7 @@ import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.highfive.backend.auth.client.dto.response.KakaoUserResponseDto;
 import org.highfive.backend.content.entity.shorts.ShortsComment;
 import org.highfive.backend.content.entity.shorts.log.ShortsLikeTimeLog;
@@ -17,6 +18,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Entity
+@Setter
 @Table(name = "users")
 @Builder
 @AllArgsConstructor
