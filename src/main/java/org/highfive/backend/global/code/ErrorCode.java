985d8ac8f6963ca0b64371500efa594d2b9ad0f2
likerhythm <wjdalsdl0412@gmail.com>
2025-07-08 02:13:37 +0900

    feat[#34]: 에러코드 추가

--- src/main/java/org/highfive/backend/global/code/ErrorCode.java
+++ src/main/java/org/highfive/backend/global/code/ErrorCode.java
@@ -10,7 +10,9 @@ public enum ErrorCode {
     BAD_REQUEST("SEVER_002", "올바르지 않은 입력값입니다.", HttpStatus.BAD_REQUEST),
     FAST_API_ERROR("40002", "FastAPI 서버에 접근할 수 없습니다.", HttpStatus.INTERNAL_SERVER_ERROR),
     KAKAO_TOKEN_ERROR("OAUTH_001", "카카오 인증 토큰 요청 오류입니다.", HttpStatus.INTERNAL_SERVER_ERROR),
-    KAKAO_USERINFO_ERROR("OAUTH_002", "카카오 유저 정보 요청 오류입니다.", HttpStatus.INTERNAL_SERVER_ERROR);
+    KAKAO_USERINFO_ERROR("OAUTH_002", "카카오 유저 정보 요청 오류입니다.", HttpStatus.INTERNAL_SERVER_ERROR),
+    USER_NOT_FOUND_ERROR("40201", "존재하지 않는 사용자입니다.", HttpStatus.NOT_FOUND)
+    ;
 
     private final String code;
     private final String message;
