8706508f3efa603049f67731825b4f4c36fc6212
sangyunpark99 <psangyun5108@gmail.com>
2025-07-07 22:02:43 +0900

    chore[#33]: 불필요한 import 구문 삭제

--- src/test/java/org/highfive/backend/BackendApplicationTests.java
+++ src/test/java/org/highfive/backend/BackendApplicationTests.java
@@ -2,7 +2,6 @@ package org.highfive.backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 class BackendApplicationTests {
