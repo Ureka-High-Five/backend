563e5a67823cfedede10582424d2cac71503ef29
sangyunpark99 <psangyun5108@gmail.com>
2025-07-07 16:40:48 +0900

    feat[#33]: 카카오 인증 Service 회원 존재 유무에 따른 분기 처리 로직 구현

---
+++ src/main/java/org/highfive/backend/auth/service/AuthService.java
@@ -0,0 +1,37 @@
+package org.highfive.backend.auth.service;
+
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.highfive.backend.auth.client.KakaoOAuthClient;
+import org.highfive.backend.auth.client.dto.response.KakaoUserResponse;
+import org.highfive.backend.auth.controller.dto.request.OAuthRequestDto;
+import org.highfive.backend.user.entity.User;
+import org.highfive.backend.user.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthService {
+
+    private final KakaoOAuthClient kakaoOAuthClient;
+    private final UserRepository userRepository;
+
+    @Transactional
+    public void login(final OAuthRequestDto OAuthRequestDto) {
+        final String code = OAuthRequestDto.code();
+        final String token = kakaoOAuthClient.requestToken(code).accessToken();
+        final KakaoUserResponse userInfo = kakaoOAuthClient.requestUser(token);
+        final String kakaoUserId = userInfo.id();
+
+        if(userRepository.existsByKakaoUserId(kakaoUserId)) {
+            // 토큰 발급
+        }
+
+        saveUser(userInfo);
+    }
+
+    private void saveUser(final KakaoUserResponse userInfo) {
+        final User user = User.from(userInfo);
+        userRepository.save(user);
+    }
+}
