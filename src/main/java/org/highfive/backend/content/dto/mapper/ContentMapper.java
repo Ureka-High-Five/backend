bebc99b721af8570781833e94ff6659ad21674a8
JIYEON HWANG <ghkdwldus0807@gmail.com>
2025-07-09 01:51:13 +0900

    feat[#55] : 콘텐츠 상세 정보 조회 기능 구현

--- src/main/java/org/highfive/backend/content/dto/mapper/ContentMapper.java
+++ src/main/java/org/highfive/backend/content/dto/mapper/ContentMapper.java
@@ -1,11 +1,21 @@
 package org.highfive.backend.content.dto.mapper;
 
+import java.util.List;
 import org.highfive.backend.content.dto.OnboardingInitContentsResponseDto;
+import org.highfive.backend.content.dto.response.ContentDetailResponseDto;
 import org.highfive.backend.content.entity.Content;
 
 public class ContentMapper {
 
     public static OnboardingInitContentsResponseDto toOnboardingInitContentsResponseDto(Content content) {
         return new OnboardingInitContentsResponseDto(content.getId(), content.getThumbnailUrl(), content.getTitle());
+    }
+
+
+    public static ContentDetailResponseDto toContentDetailResponseDto(Content content, String director,
+                                                                      List<String> actors, List<String> genres,
+                                                                      Integer rating, String review) {
+        return new ContentDetailResponseDto(content.getTitle(), genres, content.getRunningTime(), content.getGrade(),
+                rating, review, content.getPostUrl(), actors, director, content.getOpenDate().toString());
     }
 }
--- src/main/java/org/highfive/backend/content/service/ContentService.java
+++ src/main/java/org/highfive/backend/content/service/ContentService.java
@@ -1,10 +1,25 @@
 package org.highfive.backend.content.service;
 
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.highfive.backend.content.dto.OnboardingInitContentsResponseDto;
 import org.highfive.backend.content.dto.TopContentByGenreDto;
+import org.highfive.backend.content.dto.mapper.ContentMapper;
+import org.highfive.backend.content.dto.response.ContentDetailResponseDto;
+import org.highfive.backend.content.entity.Content;
+import org.highfive.backend.content.entity.metadata.MetaInfo;
+import org.highfive.backend.content.entity.metadata.MetaType;
+import org.highfive.backend.content.entity.repository.MetaInfoContentsRepository;
+import org.highfive.backend.content.entity.review.Review;
+import org.highfive.backend.content.exception.ContentErrorCode;
 import org.highfive.backend.content.repository.ContentRepository;
+import org.highfive.backend.content.repository.ReviewRepository;
+import org.highfive.backend.global.code.SuccessCode;
+import org.highfive.backend.global.dto.Response;
+import org.highfive.backend.global.exception.BusinessException;
+import org.highfive.backend.user.entity.User;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -14,6 +29,8 @@ public class ContentService {
     private static final int INIT_CONTENT_CNT = 6;
 
     private final ContentRepository contentRepository;
+    private final MetaInfoContentsRepository metaInfoContentsRepository;
+    private final ReviewRepository reviewRepository;
 
     public List<OnboardingInitContentsResponseDto> getDistinctGenreTopContents() {
         List<TopContentByGenreDto> topContents = contentRepository.findTopContentPerGenre(INIT_CONTENT_CNT);
@@ -21,5 +38,34 @@ public class ContentService {
         return topContents.stream()
                 .map(dto -> new OnboardingInitContentsResponseDto(dto.getId(), dto.getThumbnailUrl(), dto.getTitle()))
                 .toList();
+    }
+
+    public Response<ContentDetailResponseDto> getContentDetail(final Long contentId, final User user) {
+
+        Content existedContent = contentRepository.findById(contentId)
+                .orElseThrow(() -> new BusinessException(ContentErrorCode.CONTENT_NOT_FOUND));
+
+        List<MetaInfo> metaInfos = metaInfoContentsRepository.findMetaInfosByContentId(contentId);
+
+        Map<MetaType, List<String>> metaMap = metaInfos.stream()
+                .collect(Collectors.groupingBy(
+                        MetaInfo::getType,
+                        Collectors.mapping(MetaInfo::getName, Collectors.toList())
+                ));
+
+        String director = metaMap.getOrDefault(MetaType.DIRECTOR, List.of())
+                .stream().findFirst().orElse(null);
+
+        List<String> actors = metaMap.getOrDefault(MetaType.ACTOR, List.of());
+        List<String> genres = metaMap.getOrDefault(MetaType.GENRE, List.of());
+
+        Review review = reviewRepository.findByUserAndContent(user, existedContent).orElse(null);
+        Integer rating = (review != null) ? review.getRating() : null;
+        String reviewText = (review != null) ? review.getReviewText() : null;
+
+        ContentDetailResponseDto response = ContentMapper.toContentDetailResponseDto(existedContent, director, actors,
+                genres, rating, reviewText);
+
+        return new Response<>(SuccessCode.OK.getCode(), response, null);
     }
 }
