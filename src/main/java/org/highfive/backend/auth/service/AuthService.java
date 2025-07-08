5821272f25afdff937c5cee92a9eec15a4bd1dab
sangyunpark99 <psangyun5108@gmail.com>
2025-07-08 16:26:50 +0900

    feat[#44]: 유저가 회원가입을 하지 않은 경우, 한 경우 분리해서 응답값을 주는 비즈니스 로직 구현

--- src/main/java/org/highfive/backend/auth/service/AuthService.java
+++ src/main/java/org/highfive/backend/auth/service/AuthService.java
@@ -5,10 +5,15 @@ import lombok.RequiredArgsConstructor;
 import org.highfive.backend.auth.client.KakaoOAuthClient;
 import org.highfive.backend.auth.client.dto.response.KakaoUserResponseDto;
 import org.highfive.backend.auth.controller.dto.request.OAuthRequestDto;
+import org.highfive.backend.auth.controller.dto.response.TokenResponseDto;
+import org.highfive.backend.auth.jwt.JwtUtils;
 import org.highfive.backend.global.dto.Response;
+import org.highfive.backend.user.entity.Role;
 import org.highfive.backend.user.entity.User;
 import org.highfive.backend.user.repository.UserRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 import static org.highfive.backend.global.code.SuccessCode.OK;
 
@@ -18,6 +23,7 @@ public class AuthService {
 
     private final KakaoOAuthClient kakaoOAuthClient;
     private final UserRepository userRepository;
+    private final JwtUtils jwtUtils;
 
     @Transactional
     public Response<?> login(final OAuthRequestDto OAuthRequestDto) {
@@ -26,17 +32,28 @@ public class AuthService {
         final KakaoUserResponseDto userInfo = kakaoOAuthClient.requestUser(token);
         final String kakaoUserId = userInfo.id();
 
-        if(userRepository.existsByKakaoUserId(kakaoUserId)) {
-            // 토큰 발급
+        if (!userRepository.existsByKakaoUserId(kakaoUserId)) {
+            saveUser(userInfo);
+            final String nickname = userInfo.kakaoAccount().profile().nickname();
+            return nicknameResponse(nickname);
         }
 
-        saveUser(userInfo);
-        final String nickname = userInfo.kakaoAccount().profile().nickname();
-        return new Response<>(OK.getCode(), nickname, OK.getMessage());
+        return tokenResponse(kakaoUserId);
     }
 
     private void saveUser(final KakaoUserResponseDto userInfo) {
         final User user = User.from(userInfo);
         userRepository.save(user);
+    }
+
+    private Response<TokenResponseDto> tokenResponse(final String kakaoUserId) {
+        final String accessToken = jwtUtils.generateAccessToken(kakaoUserId, List.of(Role.USER.toString()));
+        final String refreshToken = jwtUtils.generateRefreshToken(kakaoUserId);
+        final TokenResponseDto tokens = new TokenResponseDto(accessToken, refreshToken);
+        return new Response<>(OK.getCode(), tokens, OK.getMessage());
+    }
+
+    private Response<String> nicknameResponse(final String nickname) {
+        return new Response<>(OK.getCode(), nickname, OK.getMessage());
     }
 }
