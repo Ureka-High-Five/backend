4c9a37f8a1db932d70129d671bc077400309553b
JIYEON HWANG <ghkdwldus0807@gmail.com>
2025-07-08 13:48:35 +0900

    feat[#39] : Review 수정 API 추가

--- src/main/java/org/highfive/backend/content/exception/ReviewErrorCode.java
+++ src/main/java/org/highfive/backend/content/exception/ReviewErrorCode.java
@@ -1,35 +1,34 @@
 package org.highfive.backend.content.exception;
 
+import lombok.RequiredArgsConstructor;
 import org.highfive.backend.global.code.ErrorCode;
 import org.springframework.http.HttpStatus;
-
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 public enum ReviewErrorCode implements ErrorCode {
 
 
-	REVIEW_001(42200,"리뷰에 부적절한 단어가 포함되어있습니다.", HttpStatus.UNPROCESSABLE_ENTITY);
+    REVIEW_001(42200, "리뷰에 부적절한 단어가 포함되어있습니다.", HttpStatus.UNPROCESSABLE_ENTITY),
+    REVIEW_002(40403, "존재하지 않는 리뷰입니다.", HttpStatus.NOT_FOUND),
+    ;
 
-	private final int code;
-	private final String message;
-	private final HttpStatus httpStatus;
+    private final int code;
+    private final String message;
+    private final HttpStatus httpStatus;
 
 
+    @Override
+    public int getCode() {
+        return this.code;
+    }
 
-	@Override
-	public int getCode() {
-		return this.code;
-	}
+    @Override
+    public String getMessage() {
+        return this.message;
+    }
 
-	@Override
-	public String getMessage() {
-		return this.message;
-	}
-
-	@Override
-	public HttpStatus getHttpStatus() {
-		return this.httpStatus;
-	}
+    @Override
+    public HttpStatus getHttpStatus() {
+        return this.httpStatus;
+    }
 }
--- src/main/java/org/highfive/backend/content/service/ReviewService.java
+++ src/main/java/org/highfive/backend/content/service/ReviewService.java
@@ -1,10 +1,16 @@
 package org.highfive.backend.content.service;
 
+import static org.highfive.backend.global.code.SuccessCode.CREATED;
+import static org.highfive.backend.global.code.SuccessCode.OK;
+
+import lombok.RequiredArgsConstructor;
 import org.highfive.backend.content.dto.mapper.ReviewMapper;
 import org.highfive.backend.content.dto.request.CreateReviewRequestDto;
+import org.highfive.backend.content.dto.request.UpdateReviewRequestDto;
 import org.highfive.backend.content.entity.Content;
 import org.highfive.backend.content.entity.review.Review;
 import org.highfive.backend.content.exception.ContentErrorCode;
+import org.highfive.backend.content.exception.ReviewErrorCode;
 import org.highfive.backend.content.repository.ContentRepository;
 import org.highfive.backend.content.repository.ReviewRepository;
 import org.highfive.backend.global.dto.Response;
@@ -12,26 +18,36 @@ import org.highfive.backend.global.exception.BusinessException;
 import org.highfive.backend.user.entity.User;
 import org.springframework.stereotype.Service;
 
-import static org.highfive.backend.global.code.SuccessCode.REVIEW_CREATED;
-
-import lombok.RequiredArgsConstructor;
-
 @Service
 @RequiredArgsConstructor
 public class ReviewService {
 
-	private final ReviewRepository reviewRepository;
-	private final ContentRepository contentRepository;
+    private final ReviewRepository reviewRepository;
+    private final ContentRepository contentRepository;
 
-	public Response<?> createReview(CreateReviewRequestDto requestDto, User user) {
+    public Response<?> createReview(CreateReviewRequestDto requestDto, User user) {
 
-		Content content = contentRepository.findById(requestDto.contentId())
-			.orElseThrow(()-> new BusinessException(ContentErrorCode.CONTENT_001));
+        // TODO: review에 대한 금칙어 처리 추가 필요
 
-		Review review = ReviewMapper.toReview(requestDto, user, content);
-		reviewRepository.save(review);
+        Content content = contentRepository.findById(requestDto.contentId())
+                .orElseThrow(() -> new BusinessException(ContentErrorCode.CONTENT_001));
 
-		return new Response<>(REVIEW_CREATED.getCode(),null,null);
+        Review review = ReviewMapper.toReview(requestDto, user, content);
+        reviewRepository.save(review);
 
-	}
+        return new Response<>(CREATED.getCode(), null, null);
+
+    }
+
+    public Response<?> updateReview(Long reviewId, UpdateReviewRequestDto requestDto, User user) {
+        // TODO: review에 대한 금칙어 처리 추가 필요
+
+        Review existedReview = reviewRepository.findById(reviewId)
+                .orElseThrow(() -> new BusinessException(ReviewErrorCode.REVIEW_002));
+
+        existedReview.updateReview(requestDto.rating, requestDto.review);
+
+        return new Response<>(OK.getCode(), null, null);
+
+    }
 }
