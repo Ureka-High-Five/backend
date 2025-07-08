775f492f5cfbae1b54988d386b9c53510b980ca7
likerhythm <wjdalsdl0412@gmail.com>
2025-07-08 23:24:38 +0900

    chore[#34]: SuccessCode 사용하도록 수정

--- src/main/java/org/highfive/backend/user/UserController.java
+++ src/main/java/org/highfive/backend/user/UserController.java
@@ -2,6 +2,7 @@ package org.highfive.backend.user;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.highfive.backend.global.code.SuccessCode;
 import org.highfive.backend.global.dto.Response;
 import org.highfive.backend.user.dto.request.SubmitOnboardingRequestDto;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -17,8 +18,8 @@ public class UserController {
     private final UserService userService;
 
     @PatchMapping("/userInfo")
-    public Response<Void> submitOnboarding(@Valid @RequestBody SubmitOnboardingRequestDto request) {
+    public Response<Void> submitOnboarding(@Valid @RequestBody final SubmitOnboardingRequestDto request) {
         userService.initUser(request);
-        return new Response<>(20000, null, null);
+        return new Response<>(SuccessCode.OK.getCode(), null, null);
     }
 }
