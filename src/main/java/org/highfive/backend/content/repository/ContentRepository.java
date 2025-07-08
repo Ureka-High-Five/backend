9c0ceaf9f8d374efc02edcb0e24fc59279811053
likerhythm <wjdalsdl0412@gmail.com>
2025-07-08 23:52:44 +0900

    feat[#34]: 여러 컨텐츠의 장르 한번에 찾는 쿼리 추가

--- src/main/java/org/highfive/backend/content/repository/ContentRepository.java
+++ src/main/java/org/highfive/backend/content/repository/ContentRepository.java
@@ -1,6 +1,7 @@
 package org.highfive.backend.content.repository;
 
 import java.util.List;
+import java.util.Map;
 import org.highfive.backend.content.dto.TopContentByGenreDto;
 import org.highfive.backend.content.entity.Content;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -32,4 +33,12 @@ public interface ContentRepository extends JpaRepository<Content,Long> {
           AND m.type = 'GENRE'
         """, nativeQuery = true)
     List<String> findGenreNamesByContentId(@Param("contentId") Long contentId); // 컨텐츠 아이디로 장르 조회
+
+
+    @Query("SELECT new map(c.id as contentId, m.name as genreName) " +
+            "FROM MetaInfoContents mic " +
+            "JOIN mic.metaInfo m " +
+            "JOIN mic.content c " +
+            "WHERE m.type = 'GENRE' AND c.id IN :contentIds")
+    List<Map<String, Object>> findContentGenresByContentIds(@Param("contentIds") List<Long> contentIds);
 }
