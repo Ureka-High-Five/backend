2c8ecb4fd9644064e43d68827dc8280c7cc11739
JIYEON HWANG <ghkdwldus0807@gmail.com>
2025-07-09 02:17:35 +0900

    fix[#55] : JPQL 자동 생성 하도록 변경

--- src/main/java/org/highfive/backend/content/entity/repository/MetaInfoContentsRepository.java
+++ src/main/java/org/highfive/backend/content/entity/repository/MetaInfoContentsRepository.java
@@ -1,16 +1,11 @@
 package org.highfive.backend.content.entity.repository;
 
 import java.util.List;
-import org.highfive.backend.content.entity.metadata.MetaInfo;
 import org.highfive.backend.content.entity.metadata.MetaInfoContents;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 public interface MetaInfoContentsRepository extends JpaRepository<MetaInfoContents, Long> {
 
-    @Query("""
-                select mi from MetaInfoContents mic join fetch mic.metaInfo mi where mic.content.id = :contentId
-            """)
-    List<MetaInfo> findMetaInfosByContentId(@Param("contentId") Long contentId);
+    List<MetaInfoContents> findByContent_Id(@Param("contentId") Long contentId);
 }
