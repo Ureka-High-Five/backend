1e0fb696404e72fa26b84ce117b9c567b0a7b716
likerhythm <wjdalsdl0412@gmail.com>
2025-07-08 11:33:48 +0900

    feat[#45]: PreferMetaInfoRepository 추가

---
+++ src/main/java/org/highfive/backend/user/entity/preference/PreferMetaInfoRepository.java
@@ -0,0 +1,6 @@
+package org.highfive.backend.user.entity.preference;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PreferMetaInfoRepository extends JpaRepository<PreferMetaInfo, Long> {
+}
