83cf17f3baa5679c9c9b4c4a96942dea26a78c5b
likerhythm <wjdalsdl0412@gmail.com>
2025-07-08 20:04:26 +0900

    chore[#34]: 불필요 줄 삭제

--- src/main/java/org/highfive/backend/content/exception/ContentErrorCode.java
+++ src/main/java/org/highfive/backend/content/exception/ContentErrorCode.java
@@ -7,7 +7,6 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 public enum ContentErrorCode implements ErrorCode {
 
-
     CONTENT_001(40402, "존재하지 않는 컨텐츠입니다.", HttpStatus.NOT_FOUND),
     ;
 
