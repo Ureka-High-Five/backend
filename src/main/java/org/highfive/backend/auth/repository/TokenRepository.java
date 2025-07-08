90270295a9ae312bee068cb96b7b8493cec43b8c
sangyunpark99 <psangyun5108@gmail.com>
2025-07-09 02:17:36 +0900

    feat[#57]: 로그아웃 시, 액세스 토큰 블랙리스트에 추가하는 비즈니스 로직, 리프레시 토큰을 삭제하는 비즈니스 로직, 주어진 액세스 토큰이 현재 블랙리스트인지 확인하는 로직 구현

--- src/main/java/org/highfive/backend/auth/repository/TokenRepository.java
+++ src/main/java/org/highfive/backend/auth/repository/TokenRepository.java
@@ -9,7 +9,9 @@ import java.util.concurrent.TimeUnit;
 
 @Service
 @RequiredArgsConstructor
-public class RefreshTokenRepository {
+public class TokenRepository {
+
+    private final String LOG_OUT = "logout";
 
     private final StringRedisTemplate redisTemplate;
 
@@ -20,8 +22,20 @@ public class RefreshTokenRepository {
         redisTemplate.opsForValue().set(String.valueOf(userId), refreshToken, refreshTokenExpiration, TimeUnit.MILLISECONDS);
     }
 
+    public void saveLogoutToken(final String accessToken, final long tokenRemainingTime) {
+        redisTemplate.opsForValue().set(accessToken, LOG_OUT, tokenRemainingTime, TimeUnit.MILLISECONDS);
+    }
+
     public String getRefreshToken(final String userId) {
         return redisTemplate.opsForValue().get(String.valueOf(userId));
+    }
+
+    public void delete(final String userId) {
+        redisTemplate.delete(String.valueOf(userId));
+    }
+
+    public boolean isBlackListToken(final String token) {
+        return redisTemplate.hasKey(token);
     }
 
     public boolean isRefreshTokenValid(final String userId, final String refreshToken) {
