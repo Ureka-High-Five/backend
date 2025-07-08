d071dfeb6e761cf434a291675614ff7b6d54c3d2
sangyunpark99 <psangyun5108@gmail.com>
2025-07-08 17:19:38 +0900

    feat[#44]: 카카오 유저 아이디를 기반으로 정보를 찾는 메서드 구현

--- src/main/java/org/highfive/backend/user/repository/UserRepository.java
+++ src/main/java/org/highfive/backend/user/repository/UserRepository.java
@@ -3,7 +3,11 @@ package org.highfive.backend.user.repository;
 import org.highfive.backend.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
 
     Boolean existsByKakaoUserId(String kakaoUserId);
+
+    Optional<User> findByKakaoUserId(String kakaoUserId);
 }
