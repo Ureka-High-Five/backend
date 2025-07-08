cbe89ac2e479f2e23c6c34e35dd47f5e0d117b36
likerhythm <wjdalsdl0412@gmail.com>
2025-07-08 19:40:40 +0900

    feat[#34]: 사용자 가중치 및 벡터 저장

--- src/main/java/org/highfive/backend/user/UserService.java
+++ src/main/java/org/highfive/backend/user/UserService.java
@@ -1,28 +1,40 @@
 package org.highfive.backend.user;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import lombok.RequiredArgsConstructor;
+import org.highfive.backend.content.entity.metadata.MetaInfo;
+import org.highfive.backend.content.entity.repository.MetaInfoRepository;
+import org.highfive.backend.content.repository.ContentRepository;
 import org.highfive.backend.global.client.fastapi.FastApiClient;
 import org.highfive.backend.global.client.fastapi.dto.FastApiOnboardingResponseDto;
 import org.highfive.backend.global.exception.BusinessException;
+import org.highfive.backend.global.util.WeightManager;
 import org.highfive.backend.user.code.UserErrorCode;
 import org.highfive.backend.user.dto.request.SubmitOnboardingRequestDto;
 import org.highfive.backend.user.entity.Gender;
 import org.highfive.backend.user.entity.User;
+import org.highfive.backend.user.entity.preference.PreferMetaInfo;
 import org.highfive.backend.user.entity.preference.PreferMetaInfoRepository;
 import org.highfive.backend.user.repository.UserRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
 
     private final FastApiClient fastApiClient;
+    private final WeightManager weightManager;
     private final UserRepository userRepository;
+    private final ContentRepository contentRepository;
+    private final MetaInfoRepository metaInfoRepository;
     private final PreferMetaInfoRepository preferMetaInfoRepository;
 
-    public void initUser(SubmitOnboardingRequestDto request) {
+
+    @Transactional
+    public void initUser(final SubmitOnboardingRequestDto request) {
         long userId = request.userId();
         User user = userRepository.findById(userId)
                 .orElseThrow(() -> new BusinessException(UserErrorCode.USER_NOT_FOUND_ERROR));
@@ -30,7 +42,7 @@ public class UserService {
         initVector(request, user);
     }
 
-    private void initBasic(SubmitOnboardingRequestDto request, User user) {
+    private void initBasic(final SubmitOnboardingRequestDto request, final User user) {
         int age = request.year();
         Gender gender = request.gender();
         String name = request.name();
@@ -39,14 +51,45 @@ public class UserService {
         user.setName(name);
     }
 
-    private void initVector(SubmitOnboardingRequestDto request, User user) {
+    private void initVector(final SubmitOnboardingRequestDto request, final User user) {
         List<Long> contentIds = request.selectedContentIds();
         FastApiOnboardingResponseDto response = fastApiClient.onboarding(contentIds);
-        String vector = response.vector(); // 사용자 벡터
-        // 사용자 가중치
 
+        // 벡터 저장
+        String vector = response.vector();
         user.setEmbedding(vector);
-        // todo 가중치 저장
-        // todo 가중치 key 매퍼 만들기
+
+        // 가중치 저장
+        Map<String, Integer> genreCount = countGenre(contentIds);
+        Map<String, Double> genreWeights = weightManager.calcWeight(genreCount);
+        saveUserWeight(user, genreWeights);
+    }
+
+    private void saveUserWeight(final User user, final Map<String, Double> genreWeights) {
+        for (Map.Entry<String, Double> entry : genreWeights.entrySet()) {
+            final String genreName = entry.getKey();
+            final double weight = entry.getValue();
+
+            final MetaInfo metaInfo = metaInfoRepository.findGenreMetaIdByName(genreName)
+                    .orElseThrow(() -> new IllegalArgumentException("장르가 존재하지 않음: " + genreName));
+
+            final PreferMetaInfo prefer = PreferMetaInfo.builder()
+                    .user(user)
+                    .weight(weight)
+                    .metaInfo(metaInfo)
+                    .build();
+            preferMetaInfoRepository.save(prefer);
+        }
+    }
+
+    private Map<String, Integer> countGenre(final List<Long> contentIds) {
+        final Map<String, Integer> genreCount = new HashMap<>();
+        for (long contentId : contentIds) {
+            List<String> genreNames = contentRepository.findGenreNamesByContentId(contentId);
+            for (String genreName : genreNames) {
+                genreCount.put(genreName, genreCount.getOrDefault(genreName, 0) + 1);
+            }
+        }
+        return genreCount;
     }
 }
