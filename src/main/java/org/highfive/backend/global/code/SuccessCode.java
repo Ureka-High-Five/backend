297357ba52a3c347c41cc6fbed458ecaa16c76c4
JIYEON HWANG <ghkdwldus0807@gmail.com>
2025-07-08 20:39:36 +0900

    feat[#49] : SuccessCode에 204 status 추가

--- src/main/java/org/highfive/backend/global/code/SuccessCode.java
+++ src/main/java/org/highfive/backend/global/code/SuccessCode.java
@@ -8,6 +8,7 @@ public enum SuccessCode {
 
     OK(20000, "요청이 정상 처리되었습니다.", HttpStatus.OK),
     CREATED(20100, "생성이 완료되었습니다.", HttpStatus.CREATED),
+    NO_CONTENT(20400, "더 이상 응답할 내용이 없습니다.", HttpStatus.NO_CONTENT),
     ;
 
     private final int code;
