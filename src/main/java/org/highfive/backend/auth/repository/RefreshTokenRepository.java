62cbee8e0aa197907ed4aac4093bce665f66d6bb
sangyunpark99 <psangyun5108@gmail.com>
2025-07-08 21:37:15 +0900

    feat[#52]: refreshToken 저장시 사용하는 Redis DB 로직 구현

---
+++ src/main/java/org/highfive/backend/auth/repository/RefreshTokenRepository.java
@@ -0,0 +1,30 @@
+package org.highfive.backend.auth.repository;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.stereotype.Service;
+
+import java.util.concurrent.TimeUnit;
+
+@Service
+@RequiredArgsConstructor
+public class RefreshTokenRepository {
+
+    private final StringRedisTemplate redisTemplate;
+
+    @Value("${jwt.refreshToken-expiration}")
+    private Long refreshTokenExpiration;
+
+    public void save(final String userId, final String refreshToken) {
+        redisTemplate.opsForValue().set(String.valueOf(userId), refreshToken, refreshTokenExpiration, TimeUnit.MILLISECONDS);
+    }
+
+    public String getRefreshToken(final String userId) {
+        return redisTemplate.opsForValue().get(String.valueOf(userId));
+    }
+
+    public void delete(final String userId) {
+        redisTemplate.delete(String.valueOf(userId));
+    }
+}
