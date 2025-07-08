8f737135584596c046a4f8fc0139e2efcab5a75f
JIYEON HWANG <ghkdwldus0807@gmail.com>
2025-07-09 02:20:00 +0900

    fix[#55] : MetaInfoContents 엔티티에 Getter 추가

--- src/main/java/org/highfive/backend/content/entity/metadata/MetaInfoContents.java
+++ src/main/java/org/highfive/backend/content/entity/metadata/MetaInfoContents.java
@@ -1,19 +1,28 @@
 package org.highfive.backend.content.entity.metadata;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.highfive.backend.content.entity.Content;
 
 @Entity
+@Getter
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class MetaInfoContents {
 
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @ManyToOne(fetch = FetchType.LAZY)
