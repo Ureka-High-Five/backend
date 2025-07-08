9d47d5390d64b9e3b390c186d8bbb80a1be1a380
sangyunpark99 <psangyun5108@gmail.com>
2025-07-08 17:15:27 +0900

    chore[#44]: 사용하지 않는 controller 삭제

--- src/main/java/org/highfive/backend/auth/controller/AuthController.java
+++ src/main/java/org/highfive/backend/auth/controller/AuthController.java
@@ -4,10 +4,10 @@ import lombok.RequiredArgsConstructor;
 import org.highfive.backend.auth.controller.dto.request.OAuthRequestDto;
 import org.highfive.backend.auth.service.AuthService;
 import org.highfive.backend.global.dto.Response;
-import org.highfive.backend.user.entity.User;
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/auth")
@@ -19,17 +19,5 @@ public class AuthController {
     @PostMapping
     public Response<?> login(@RequestBody final OAuthRequestDto OAuthRequestDto) {
         return authService.login(OAuthRequestDto);
-    }
-
-    @GetMapping
-    public void tokenTest(@AuthenticationPrincipal final User user) {
-        System.out.println(user);
-    }
-
-    @PreAuthorize("hasRole('ADMIN')")
-    @GetMapping("/admin")
-    public void adminTest(@AuthenticationPrincipal final User user) {
-        System.out.println(user);
-        System.out.println("adminTest");
     }
 }
