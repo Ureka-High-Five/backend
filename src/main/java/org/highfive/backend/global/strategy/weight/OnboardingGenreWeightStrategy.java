3715aa6d90003512df467a77aff50253fbb5b97f
likerhythm <wjdalsdl0412@gmail.com>
2025-07-08 16:13:53 +0900

    feat[#46]: 온보딩 가중치 전략 구현
    1차 MVP용이라서 장르 선택 당 weight를 0.1로 러프하게 설정했습니다.

--- src/main/java/org/highfive/backend/global/strategy/weight/OnboardingGenreWeightStrategy.java
+++ src/main/java/org/highfive/backend/global/strategy/weight/OnboardingGenreWeightStrategy.java
@@ -1,12 +1,20 @@
 package org.highfive.backend.global.strategy.weight;
 
-import java.util.List;
 import java.util.Map;
+import org.highfive.backend.global.strategy.util.GenreHolder;
 
 public class OnboardingGenreWeightStrategy implements GenreWeightStrategy {
 
+    private final double BASIC_WEIGHT = 0.1;
+
     @Override
     public Map<String, Double> calcWeight(Map<String, Integer> genreCount) {
-        return Map.of();
+        Map<String, Double> weightMap = GenreHolder.toWeightMap();
+        for (Map.Entry<String, Integer> entry : genreCount.entrySet()) {
+            String genreName = entry.getKey();
+            int count = entry.getValue();
+            weightMap.put(genreName, count * BASIC_WEIGHT);
+        }
+        return weightMap;
     }
 }
