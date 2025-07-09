592999138260efabf47db5c174b21df343deb80a
likerhythm <wjdalsdl0412@gmail.com>
2025-07-09 13:52:11 +0900

    refactor[#64]: OnboardingController 생성
    기존 ContentController에 존재하던 온보딩 관련 api를 OnboardingController로 이전했습니다.

--- src/main/java/org/highfive/backend/content/controller/OnboardingController.java
+++ src/main/java/org/highfive/backend/content/controller/OnboardingController.java
@@ -2,29 +2,29 @@ package org.highfive.backend.content.controller;
 
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.highfive.backend.content.dto.OnboardingInitContentsResponseDto;
-import org.highfive.backend.content.dto.response.ContentDetailResponseDto;
 import org.highfive.backend.content.service.ContentService;
 import org.highfive.backend.global.code.SuccessCode;
 import org.highfive.backend.global.dto.Response;
-import org.highfive.backend.user.entity.User;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
-@RequestMapping("/content")
 @RequiredArgsConstructor
-public class ContentController {
+public class OnboardingController {
 
     private final ContentService contentService;
 
-    @GetMapping("{contentId}/detail")
-    public Response<ContentDetailResponseDto> getContentDetail(
-            @PathVariable final Long contentId,
-            @AuthenticationPrincipal final User user) {
-        return contentService.getContentDetail(contentId, user);
+    /**
+     * 온보딩 초기 6개의 작품을 띄웁니다.
+     *
+     * @return
+     */
+    @GetMapping("/content/init")
+    public Response<List<OnboardingInitContentsResponseDto>> onboardingInitContents() {
+        return new Response<>(SuccessCode.OK.getCode(), contentService.getDistinctGenreTopContents(), null);
     }
 }
