3ae1b0f3ce8118d0f5c7b1e0c59c59ebf9c6eb84
likerhythm <wjdalsdl0412@gmail.com>
2025-07-09 20:39:21 +0900

    feat[#64]: jpql용 dto 추가

---
+++ src/main/java/org/highfive/backend/content/dto/response/OnboardingContentDto.java
@@ -0,0 +1,11 @@
+package org.highfive.backend.content.dto.response;
+
+import java.time.LocalDateTime;
+
+public record OnboardingContentDto(
+        Long id,
+        String postUrl,
+        String title,
+        LocalDateTime openDate
+) {
+}
