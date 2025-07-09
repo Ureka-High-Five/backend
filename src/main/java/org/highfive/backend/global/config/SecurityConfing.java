9ef560798c1d17fa638784acf31e9561b6ef4a3f
sangyunpark99 <psangyun5108@gmail.com>
2025-07-09 19:30:01 +0900

    fix[#71]: CORS 문제 수정 5173 포트 허용

--- src/main/java/org/highfive/backend/global/config/SecurityConfing.java
+++ src/main/java/org/highfive/backend/global/config/SecurityConfing.java
@@ -6,6 +6,7 @@ import org.highfive.backend.auth.jwt.JwtAuthenticationFilter;
 import org.highfive.backend.auth.service.TokenService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -23,6 +24,7 @@ public class SecurityConfing {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
+                .cors(Customizer.withDefaults())
                 .csrf(AbstractHttpConfigurer::disable)
                 .formLogin(AbstractHttpConfigurer::disable)
                 .httpBasic(AbstractHttpConfigurer::disable)
