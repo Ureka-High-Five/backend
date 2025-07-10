bd5669043008e8456c1a9e8a1ae91536ef6be350
likerhythm <wjdalsdl0412@gmail.com>
2025-07-10 10:42:34 +0900

    refactor[#73]: FastAPI dto 명시 및 불필요 컬럼 삭제

---
+++ src/main/java/org/highfive/backend/global/client/fastapi/dto/FastApiRecommendResponseDto.java
@@ -0,0 +1,6 @@
+package org.highfive.backend.global.client.fastapi.dto;
+
+public record FastApiRecommendResponseDto(
+        long id
+) {
+}
--- src/main/java/org/highfive/backend/global/client/fastapi/dto/RecommendContentsResponseDto.java
+++
@@ -1,9 +0,0 @@
-package org.highfive.backend.global.client.fastapi.dto;
-
-public record RecommendContentsResponseDto(
-        long id,
-        String title,
-        String thumbnailUrl,
-        double similarity
-) {
-}
