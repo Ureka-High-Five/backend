1ea7b8f5b73b870e264043cde0d4958562e72c88
sangyunpark99 <psangyun5108@gmail.com>
2025-07-10 20:33:21 +0900

    chore[#75]: 메서드명 카멜 케이스로 통일

--- src/main/java/org/highfive/backend/content/entity/repository/MetaInfoContentsRepository.java
+++ src/main/java/org/highfive/backend/content/entity/repository/MetaInfoContentsRepository.java
@@ -3,9 +3,8 @@ package org.highfive.backend.content.entity.repository;
 import java.util.List;
 import org.highfive.backend.content.entity.metadata.MetaInfoContents;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.query.Param;
 
 public interface MetaInfoContentsRepository extends JpaRepository<MetaInfoContents, Long> {
 
-    List<MetaInfoContents> findByContent_Id(@Param("contentId") Long contentId);
+    List<MetaInfoContents> findByContentId(Long contentId);
 }
