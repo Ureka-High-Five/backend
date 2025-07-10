2a1c684c00292399a99c7090f1e7dea970b41d87
likerhythm <wjdalsdl0412@gmail.com>
2025-07-10 10:43:30 +0900

    feat[#73]: 사용자가 선호하는 장르들 가져오는 기능 추가

--- src/main/java/org/highfive/backend/user/entity/preference/PreferMetaInfoRepository.java
+++ src/main/java/org/highfive/backend/user/entity/preference/PreferMetaInfoRepository.java
@@ -1,6 +1,22 @@
 package org.highfive.backend.user.entity.preference;
 
+import java.util.List;
+import org.highfive.backend.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface PreferMetaInfoRepository extends JpaRepository<PreferMetaInfo, Long> {
+
+    @Query(value = """
+    SELECT mi.name
+    FROM prefer_meta_info pmi
+    JOIN meta_info mi ON pmi.meta_info_id = mi.id
+    WHERE pmi.user_id = :userId
+      AND mi.type = 'GENRE'
+    ORDER BY pmi.weight DESC
+    LIMIT :count
+""", nativeQuery = true)
+    List<String> findPreferGenresByUser(@Param("userId") Long userId, @Param("count") int count);
+
 }
