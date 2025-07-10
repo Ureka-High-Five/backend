b8ae3b61c9f21d1d300f89054946ceee40be6c3a
sangyunpark99 <psangyun5108@gmail.com>
2025-07-10 20:37:48 +0900

    chore[#75]: 불필요한 코드 삭제

--- src/main/java/org/highfive/backend/content/dto/mapper/ContentMapper.java
+++ src/main/java/org/highfive/backend/content/dto/mapper/ContentMapper.java
@@ -1,18 +1,13 @@
 package org.highfive.backend.content.dto.mapper;
 
-import java.util.List;
-import org.highfive.backend.content.dto.response.OnboardingInitContentsResponseDto;
 import org.highfive.backend.content.dto.response.ContentDetailResponseDto;
 import org.highfive.backend.content.dto.response.SearchContentResponseDto;
 import org.highfive.backend.content.entity.Content;
 import org.highfive.backend.global.dto.CursorPageResponse;
 
+import java.util.List;
+
 public class ContentMapper {
-
-    public static OnboardingInitContentsResponseDto toOnboardingInitContentsResponseDto(Content content) {
-        return new OnboardingInitContentsResponseDto(content.getId(), content.getThumbnailUrl(), content.getTitle());
-    }
-
 
     public static ContentDetailResponseDto toContentDetailResponseDto(Content content, String director,
                                                                       List<String> actors, List<String> genres) {
