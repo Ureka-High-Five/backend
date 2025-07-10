096da2d8afca71cdaf2bddf0b56435b8d660cbcb
sangyunpark99 <psangyun5108@gmail.com>
2025-07-10 20:30:25 +0900

    feat[#75]: BusinessException을 다루는 handler 에러 로그 구현

--- src/main/java/org/highfive/backend/global/exception/handler/GlobalExceptionHandler.java
+++ src/main/java/org/highfive/backend/global/exception/handler/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package org.highfive.backend.global.exception.handler;
 
+import lombok.extern.slf4j.Slf4j;
 import org.highfive.backend.global.code.ErrorCode;
 import org.highfive.backend.global.exception.BusinessException;
 import org.highfive.backend.global.exception.ErrorResponseDto;
@@ -7,12 +8,14 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+@Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(BusinessException.class)
-    public ResponseEntity<ErrorResponseDto> handleBusinessException(final BusinessException ex) {
-        final ErrorCode errorCode = ex.getErrorCode();
+    public ResponseEntity<ErrorResponseDto> handleBusinessException(final BusinessException e) {
+        final ErrorCode errorCode = e.getErrorCode();
+        log.error("{}", errorCode.getMessage(), e);
         return ResponseEntity.status(errorCode.getHttpStatus()).body(new ErrorResponseDto(errorCode));
     }
 }
