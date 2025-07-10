8ccc3f1b366324b6ab7229723bfdab419108252f
sangyunpark99 <psangyun5108@gmail.com>
2025-07-10 20:59:17 +0900

    chore[#75]: 불필요한 메서드 제거

--- src/main/java/org/highfive/backend/content/dto/mapper/ContentMapper.java
+++ src/main/java/org/highfive/backend/content/dto/mapper/ContentMapper.java
@@ -2,7 +2,6 @@ package org.highfive.backend.content.dto.mapper;
 
 import org.highfive.backend.content.dto.response.ContentDetailResponseDto;
 import org.highfive.backend.content.dto.response.SearchContentResponseDto;
-import org.highfive.backend.content.dto.response.OnboardingSelectContentResponseDto;
 import org.highfive.backend.content.entity.Content;
 import org.highfive.backend.global.dto.CursorPageResponse;
 
@@ -25,9 +24,6 @@ public class ContentMapper {
                 .map(ContentMapper::toSearchContentResponseDto)
                 .toList();
         return new CursorPageResponse<>(results, String.valueOf(nextCursor));
-    
-    
-    public static OnboardingSelectContentResponseDto toOnboardingSelectContentResponseDto(Content content) {
-        return new OnboardingSelectContentResponseDto(content.getId(), content.getPostUrl(), content.getTitle(), content.getOpenDate().getYear());
+
     }
 }
