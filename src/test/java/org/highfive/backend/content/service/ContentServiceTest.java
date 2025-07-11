dee8dfdd1baa01697712457ec6d8f9af06da2fb4
JIYEON HWANG <ghkdwldus0807@gmail.com>
2025-07-11 17:05:02 +0900

    test[#74] : 컨텐츠 상세정보 조회 service 단위 테스트 추가

---
+++ src/test/java/org/highfive/backend/content/service/ContentServiceTest.java
@@ -0,0 +1,98 @@
+package org.highfive.backend.content.service;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.when;
+
+import java.util.List;
+import java.util.Optional;
+import org.highfive.backend.common.fixture.ContentFixture;
+import org.highfive.backend.common.fixture.MetaInfoContentsFixture;
+import org.highfive.backend.common.fixture.MetaInfoFixture;
+import org.highfive.backend.content.dto.response.ContentDetailResponseDto;
+import org.highfive.backend.content.entity.Content;
+import org.highfive.backend.content.entity.metadata.MetaInfo;
+import org.highfive.backend.content.entity.metadata.MetaInfoContents;
+import org.highfive.backend.content.entity.metadata.MetaType;
+import org.highfive.backend.content.entity.repository.MetaInfoContentsRepository;
+import org.highfive.backend.content.exception.ContentErrorCode;
+import org.highfive.backend.content.repository.ContentRepository;
+import org.highfive.backend.global.code.SuccessCode;
+import org.highfive.backend.global.dto.Response;
+import org.highfive.backend.global.exception.BusinessException;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class ContentServiceTest {
+
+    @Mock
+    private ContentRepository contentRepository;
+
+    @Mock
+    private MetaInfoContentsRepository metaInfoContentsRepository;
+
+    @InjectMocks
+    private ContentService contentService;
+
+    @Test
+    @DisplayName("존재하는 content일 경우 content의 상세 정보를 반환한다")
+    public void getContentDetail_correctContentId() {
+
+        //given
+        Long contentId = 1L;
+        Content content = ContentFixture.creatContent(contentId);
+
+        MetaInfo directorInfo = MetaInfoFixture.createMetaInfo("감독", MetaType.DIRECTOR);
+        MetaInfo actorInfo = MetaInfoFixture.createMetaInfo("배우1", MetaType.ACTOR);
+        MetaInfo actorInfo2 = MetaInfoFixture.createMetaInfo("배우2", MetaType.ACTOR);
+        MetaInfo genreInfo = MetaInfoFixture.createMetaInfo("로맨스", MetaType.GENRE);
+
+        List<MetaInfoContents> metaInfoContents = List.of(
+                MetaInfoContentsFixture.createMetaInfoContents(directorInfo, content),
+                MetaInfoContentsFixture.createMetaInfoContents(actorInfo, content),
+                MetaInfoContentsFixture.createMetaInfoContents(actorInfo2, content),
+                MetaInfoContentsFixture.createMetaInfoContents(genreInfo, content)
+        );
+
+        when(contentRepository.findById(contentId)).thenReturn(Optional.of(content));
+        when(metaInfoContentsRepository.findByContentId(contentId)).thenReturn(metaInfoContents);
+
+        //when
+        Response<ContentDetailResponseDto> response = contentService.getContentDetail(contentId);
+
+        //then
+        assertEquals(SuccessCode.OK.getCode(), response.code());
+
+        ContentDetailResponseDto result = response.content();
+        assertNotNull(result);
+        assertEquals("감독", result.director());
+        assertEquals(List.of("배우1", "배우2"), result.actors());
+        assertEquals(List.of("로맨스"), result.contentGenres());
+        assertEquals("2025-07-10T00:00", result.openDate());
+        assertEquals("테스트 제목", result.contentTitle());
+
+    }
+
+    @Test
+    @DisplayName("컨텐츠가 없으면 예외를 던진다")
+    public void getContentDetail_noContentId() {
+        //given
+        Long contentId = 93498579L;
+        when(contentRepository.findById(contentId)).thenReturn(Optional.empty());
+
+        //when
+        BusinessException exception = assertThrows(BusinessException.class, () -> {
+            contentService.getContentDetail(contentId);
+        });
+
+        //then
+        assertEquals(ContentErrorCode.CONTENT_NOT_FOUND, exception.getErrorCode());
+
+    }
+}
