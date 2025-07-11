4c0bf9bdf6636cb862536fcf8ff3e6e2e0d4eba8
likerhythm <wjdalsdl0412@gmail.com>
2025-07-11 15:10:43 +0900

    fix[#73]: merge 충돌 대응

--- src/main/java/org/highfive/backend/content/controller/ContentController.java
+++ src/main/java/org/highfive/backend/content/controller/ContentController.java
@@ -17,11 +17,7 @@ import org.highfive.backend.global.dto.CursorPageResponse;
 import org.highfive.backend.global.dto.Response;
 import org.highfive.backend.user.entity.User;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/content")
