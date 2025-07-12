0213f20a24ae8658aa198af35d3e81a30671e8fc
JIYEON HWANG <ghkdwldus0807@gmail.com>
2025-07-12 16:53:53 +0900

    refactor[#95] : 에러코드를 좀 더 명확하게 구분

--- src/main/java/org/highfive/backend/content/exception/ReviewErrorCode.java
+++ src/main/java/org/highfive/backend/content/exception/ReviewErrorCode.java
@@ -11,6 +11,7 @@ public enum ReviewErrorCode implements ErrorCode {
     REVIEW_CONTAINS_PROHIBITED_WORD(42200, "리뷰에 부적절한 단어가 포함되어있습니다.", HttpStatus.UNPROCESSABLE_ENTITY),
     REVIEW_NOT_FOUND(40403, "존재하지 않는 리뷰입니다.", HttpStatus.NOT_FOUND),
     REVIEW_FORBIDDEN(40301, "해당 리뷰에 대한 권한이 없는 사용자입니다.", HttpStatus.FORBIDDEN),
+    MY_REVIEW_NOT_FOUND(40405, "나의 리뷰가 존재하지 않습니다.", HttpStatus.NOT_FOUND),
     ;
 
     private final int code;
