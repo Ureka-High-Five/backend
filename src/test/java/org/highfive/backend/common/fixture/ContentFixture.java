0f6f1187d3a1bff63413f0355a9b61ced8dcdc78
likerhythm <wjdalsdl0412@gmail.com>
2025-07-12 10:47:17 +0900

    test[#81]: 파라미터가 추가된 컨텐츠 빌더 추가

--- src/test/java/org/highfive/backend/common/fixture/ContentFixture.java
+++ src/test/java/org/highfive/backend/common/fixture/ContentFixture.java
@@ -23,5 +23,20 @@ public class ContentFixture {
                 .build();
     }
 
-
+    public static Content createContentByPopularity(Long contentId, String title, int popularity) {
+        return Content.builder()
+                .id(contentId)
+                .title(title)
+                .description("테스트 설명")
+                .videoUrl("s3://video.mp4")
+                .thumbnailUrl("s3://thumbnail.jpg")
+                .postUrl("s3://postUrl.jpg")
+                .openDate(LocalDateTime.of(2025, 7, 10, 0, 0))
+                .runningTime(120)
+                .grade(15)
+                .contentType(ContentType.MOVIE)
+                .embedding("test-embedding")
+                .popularity(popularity)
+                .build();
+    }
 }
