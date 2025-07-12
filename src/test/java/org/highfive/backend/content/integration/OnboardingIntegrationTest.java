270df75b08302d2a0e4b6564ad4b60e28bec7b1e
likerhythm <wjdalsdl0412@gmail.com>
2025-07-12 12:48:12 +0900

    test[#81]: 온보딩 초기 컨텐츠 조회 통합 테스트

---
+++ src/test/java/org/highfive/backend/content/integration/OnboardingIntegrationTest.java
@@ -0,0 +1,87 @@
+package org.highfive.backend.content.integration;
+
+import org.highfive.backend.BackendApplication;
+import org.highfive.backend.common.fixture.ContentFixture;
+import org.highfive.backend.content.dto.response.OnboardingInitContentsResponseDto;
+import org.highfive.backend.content.entity.Content;
+import org.highfive.backend.content.entity.metadata.MetaInfo;
+import org.highfive.backend.content.entity.metadata.MetaInfoContents;
+import org.highfive.backend.content.entity.metadata.MetaType;
+import org.highfive.backend.content.entity.repository.MetaInfoContentsRepository;
+import org.highfive.backend.content.entity.repository.MetaInfoRepository;
+import org.highfive.backend.content.repository.ContentRepository;
+import org.highfive.backend.global.dto.Response;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(
+        classes = BackendApplication.class,
+        webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT
+)
+@ActiveProfiles("test")
+class OnboardingIntegrationTest {
+
+    @Autowired
+    TestRestTemplate restTemplate;
+
+    @LocalServerPort
+    int port;
+
+    @Test
+    @DisplayName("온보딩 초기 콘텐츠 6개 – H2 통합 테스트")
+    void onboardingInitContentsTest() {
+        //given
+        insertSampleContents();
+
+        // when
+        String url = "http://localhost:" + port + "/content/init";
+        Response<List<OnboardingInitContentsResponseDto>> res = restTemplate.getForObject(url, Response.class);
+
+        // then
+        assertThat(res.code()).isEqualTo(20000);
+        List<OnboardingInitContentsResponseDto> data = res.content();
+        assertThat(data).hasSize(6);
+    }
+
+    @Autowired
+    private ContentRepository contentRepo;
+    @Autowired
+    private MetaInfoRepository metaRepo;
+    @Autowired
+    private MetaInfoContentsRepository micRepo;
+
+    private void insertSampleContents() {
+        MetaInfo action   = metaRepo.save(new MetaInfo(null, "Action",   MetaType.GENRE, null));
+        MetaInfo comedy   = metaRepo.save(new MetaInfo(null, "Comedy",   MetaType.GENRE, null));
+        MetaInfo thriller = metaRepo.save(new MetaInfo(null, "Thriller", MetaType.GENRE, null));
+        MetaInfo horror   = metaRepo.save(new MetaInfo(null, "Horror",   MetaType.GENRE, null));
+        MetaInfo sf       = metaRepo.save(new MetaInfo(null, "SF",       MetaType.GENRE, null));
+        MetaInfo drama    = metaRepo.save(new MetaInfo(null, "Drama",    MetaType.GENRE, null));
+        MetaInfo kids     = metaRepo.save(new MetaInfo(null, "Kids",     MetaType.GENRE, null));
+
+        Content c1 = contentRepo.save(ContentFixture.createContentByPopularity(null, "c1", 10));
+        micRepo.save(new MetaInfoContents(null, action, c1));
+        Content c2 = contentRepo.save(ContentFixture.createContentByPopularity(null, "c2", 20));
+        micRepo.save(new MetaInfoContents(null, comedy, c2));
+        Content c3 = contentRepo.save(ContentFixture.createContentByPopularity(null, "c3", 30));
+        micRepo.save(new MetaInfoContents(null, thriller, c3));
+        Content c4 = contentRepo.save(ContentFixture.createContentByPopularity(null, "c4", 40));
+        micRepo.save(new MetaInfoContents(null, horror, c4));
+        Content c5 = contentRepo.save(ContentFixture.createContentByPopularity(null, "c5", 50));
+        micRepo.save(new MetaInfoContents(null, sf, c5));
+        Content c6 = contentRepo.save(ContentFixture.createContentByPopularity(null, "c6", 60));
+        micRepo.save(new MetaInfoContents(null, drama, c6));
+        Content c7 = contentRepo.save(ContentFixture.createContentByPopularity(null, "c7", 70));
+        micRepo.save(new MetaInfoContents(null, kids, c7));
+
+    }
+}
