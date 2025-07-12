14476dd9e0c4b389a2d8d1f3cc989ed4c6e5e126
likerhythm <wjdalsdl0412@gmail.com>
2025-07-13 01:07:03 +0900

    test[#108]: Bad Request 예외 테스트

--- src/test/java/org/highfive/backend/user/controller/UserControllerTest.java
+++ src/test/java/org/highfive/backend/user/controller/UserControllerTest.java
@@ -68,3 +68,33 @@ class UserControllerTest {
 
     }
 
+    @Test
+    @DisplayName("온보딩 제출 - 10개 미만의 작품을 선택할 경우 Bad Request가 발생합니다")
+    void submitOnboarding_fail_lessThan10Contents() throws Exception {
+        // given
+        SubmitOnboardingRequestDto req =
+                new SubmitOnboardingRequestDto(
+                        1L,
+                        List.of(1L, 2L, 3L, 4L, 5L, 6L, 7L, 8L, 9L),
+                        1998,
+                        Gender.MALE,
+                        "홍길동"
+                );
+
+        TokenResponseDto tokens = new TokenResponseDto(
+                "access-token",
+                "refresh-token",
+                true
+        );
+        Response<TokenResponseDto> serviceRes = new Response<>(20000, tokens, "요청이 정상 처리되었습니다.");
+
+        given(userService.initUser(req)).willReturn(serviceRes);
+
+        // when, then
+        mockMvc.perform(
+                        patch("/user/info")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(objectMapper.writeValueAsString(req)))
+                .andExpect(status().isBadRequest());
+    }
+}
