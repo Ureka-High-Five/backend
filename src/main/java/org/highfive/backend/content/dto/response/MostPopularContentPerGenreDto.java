c0cfaa91746242758179dc70d86c776bfd570f84
likerhythm <wjdalsdl0412@gmail.com>
2025-07-12 10:50:44 +0900

    test[#81]: 장르별 1위 컨텐츠 6개 조회 테스트

--- src/main/java/org/highfive/backend/content/dto/response/MostPopularContentPerGenreDto.java
+++ src/main/java/org/highfive/backend/content/dto/response/MostPopularContentPerGenreDto.java
@@ -4,6 +4,7 @@ public record MostPopularContentPerGenreDto(
         Long id,
         String thumbnailUrl,
         String title,
+        int popularity,
         String genreName,
         Long rank
 ) {}
