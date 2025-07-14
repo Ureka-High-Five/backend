1c7acc7488593ea81d9620607dca0d71f8a4c8be
likerhythm <wjdalsdl0412@gmail.com>
2025-07-14 16:53:37 +0900

    feat[#123]: genres, actors, director 필드 추가

--- src/main/java/org/highfive/backend/content/dto/request/AdminAddContentRequestDto.java
+++ src/main/java/org/highfive/backend/content/dto/request/AdminAddContentRequestDto.java
@@ -1,16 +1,20 @@
 package org.highfive.backend.content.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Positive;
-import jakarta.validation.constraints.PositiveOrZero;
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Builder
 @NoArgsConstructor
+@AllArgsConstructor
 public class AdminAddContentRequestDto {
 
     @NotBlank
@@ -39,4 +43,16 @@ public class AdminAddContentRequestDto {
 
     @NotBlank
     private String type;
+
+    @NotEmpty
+    private List<String> genres;
+
+    @NotEmpty
+    private List<String> actors;
+
+    @NotBlank
+    private String director;
 }
+
+
+
