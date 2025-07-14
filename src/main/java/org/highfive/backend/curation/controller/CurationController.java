d8e866de9f8203c1478933156b352ea0f0377f65
sangyunpark99 <psangyun5108@gmail.com>
2025-07-14 14:39:28 +0900

    feat[#109]: RequestMapping으로 content url 추가

--- src/main/java/org/highfive/backend/curation/controller/CurationController.java
+++ src/main/java/org/highfive/backend/curation/controller/CurationController.java
@@ -9,9 +9,11 @@ import org.highfive.backend.user.entity.User;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/curation")
 @RequiredArgsConstructor
 public class CurationController {
 
