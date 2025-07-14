a34a15d6c711ebe16c8f933e5317844c4bb94441
likerhythm <wjdalsdl0412@gmail.com>
2025-07-14 16:54:46 +0900

    feat[#123]: MetaInfo 에러코드 추가

--- src/main/java/org/highfive/backend/content/exception/MetaInfoErrorCode.java
+++ src/main/java/org/highfive/backend/content/exception/MetaInfoErrorCode.java
@@ -8,7 +8,9 @@ import org.springframework.http.HttpStatus;
 public enum MetaInfoErrorCode implements ErrorCode {
     
     GENRE_NOT_FOUND(40404, "존재하지 않는 장르입니다.", HttpStatus.NOT_FOUND),
-    COUNTRY_NOT_FOUND(40406, "존재하지 않는 나라입니다.", HttpStatus.NOT_FOUND)
+    COUNTRY_NOT_FOUND(40406, "존재하지 않는 나라입니다.", HttpStatus.NOT_FOUND),
+    ACTOR_NOT_FOUND(40407, "존재하지 않는 배우입니다.", HttpStatus.NOT_FOUND),
+    DIRECTOR_NOT_FOUND(40408, "존재하지 않는 감독입니다.", HttpStatus.NOT_FOUND),
     ;
 
     private final int code;
