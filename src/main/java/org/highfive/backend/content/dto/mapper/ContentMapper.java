131476a81fcb2d79d112350b05981f17fa0a9b8c
likerhythm <wjdalsdl0412@gmail.com>
2025-07-14 16:54:12 +0900

    fix[#123]: ContentMapper opendate 파싱 오류 해결

--- src/main/java/org/highfive/backend/content/dto/mapper/ContentMapper.java
+++ src/main/java/org/highfive/backend/content/dto/mapper/ContentMapper.java
@@ -1,5 +1,6 @@
 package org.highfive.backend.content.dto.mapper;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import org.highfive.backend.content.dto.request.AdminAddContentRequestDto;
 import org.highfive.backend.content.dto.response.ContentDetailResponseDto;
@@ -36,7 +37,7 @@ public class ContentMapper {
                 .description(request.getDescription())
                 .videoUrl(request.getVideoUrl())
                 .postUrl(request.getPostUrl())
-                .openDate(LocalDateTime.parse(request.getOpenDate()))
+                .openDate(LocalDateTime.from(LocalDate.parse(request.getOpenDate()).atStartOfDay()))
                 .runningTime(request.getRunningTime())
                 .totalRound(request.getTotalRound())
                 .contentType(ContentType.valueOf(request.getType()))
