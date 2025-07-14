5043d614be84c2b69eb6e3dbfb6efb09d25f4cdc
likerhythm <wjdalsdl0412@gmail.com>
2025-07-14 11:21:10 +0900

    feat[#123]: 컨텐츠 추가 - 컨트롤러 계층 구현

---
+++ src/main/java/org/highfive/backend/content/controller/AdminContentController.java
@@ -0,0 +1,22 @@
+package org.highfive.backend.content.controller;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.highfive.backend.content.dto.request.AdminAddContentRequestDto;
+import org.highfive.backend.content.service.AdminContentService;
+import org.highfive.backend.global.dto.Response;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class AdminContentController {
+
+    private final AdminContentService adminContentService;
+
+    @PostMapping("/content")
+    public Response<Void> adminAddContent(@Valid @RequestBody AdminAddContentRequestDto request) {
+        return adminContentService.addContent(request);
+    }
+}
