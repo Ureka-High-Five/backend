c837f64c8b4791dc08f7e1c602c9113321ff26ae
sangyunpark99 <psangyun5108@gmail.com>
2025-07-14 16:59:13 +0900

    feat[#127]: CurationDetailResponseDto로 변환해주는 mapper 메서드 구현

--- src/main/java/org/highfive/backend/curation/dto/mapper/CurationMapper.java
+++ src/main/java/org/highfive/backend/curation/dto/mapper/CurationMapper.java
@@ -1,8 +1,12 @@
 package org.highfive.backend.curation.dto.mapper;
 
+import org.highfive.backend.content.dto.response.HomeContentsResponseDto;
 import org.highfive.backend.curation.dto.request.CreateCurationRequestDto;
+import org.highfive.backend.curation.dto.response.CurationDetailResponseDto;
 import org.highfive.backend.curation.entity.Curation;
 import org.highfive.backend.user.entity.User;
+
+import java.util.List;
 
 public class CurationMapper {
 
@@ -13,5 +17,16 @@ public class CurationMapper {
                 .description(dto.description())
                 .thumbnailUrl(dto.thumbnail())
                 .build();
+    }
+
+    public static CurationDetailResponseDto toCurationDetailResponseDto(final Curation curation, final List<CurationDetailResponseDto.ContentDto> contentDtos) {
+        return new CurationDetailResponseDto(
+                curation.getTitle(),
+                contentDtos,
+                curation.getThumbnailUrl(),
+                curation.getUser().getProfileUrl(),
+                curation.getUser().getName(),
+                curation.getUser().getId()
+        );
     }
 }
