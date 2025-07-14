d3c7aa5f8e0934a0d2efba72305ccb09603263ec
likerhythm <wjdalsdl0412@gmail.com>
2025-07-14 21:28:30 +0900

    feat[#133]: 어떤 한 컨텐츠의 장르 가져오는 쿼리 추가

--- src/main/java/org/highfive/backend/content/repository/jpa/MetaInfoRepository.java
+++ src/main/java/org/highfive/backend/content/repository/jpa/MetaInfoRepository.java
@@ -1,5 +1,6 @@
 package org.highfive.backend.content.repository.jpa;
 
+import java.util.List;
 import org.highfive.backend.content.entity.metadata.MetaInfo;
 import org.highfive.backend.content.entity.metadata.MetaType;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -10,4 +11,12 @@ public interface MetaInfoRepository extends JpaRepository<MetaInfo, Long> {
 
     @Query(" SELECT mi FROM MetaInfo mi WHERE mi.name = :name AND mi.type = :type ")
     MetaInfo findByNameAndType(@Param("name") String name, @Param("type") MetaType type);
+
+    @Query("""
+            SELECT mi.name
+            FROM MetaInfo mi
+            JOIN MetaInfoContents mic ON mi.id = mic.metaInfo.id
+            WHERE mi.type = 'GENRE' and mic.content.id = :contentId
+            """)
+    List<String> findGenresByContentId(Long contentId);
 }
