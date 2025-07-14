1f3bee2444ddf084ed7a274c4b5a08f450abebc4
sangyunpark99 <psangyun5108@gmail.com>
2025-07-14 21:24:39 +0900

    feat[#134]: 나의 큐레이션 정보를 가져오는 서비스 비즈니스 로직 구현

--- src/main/java/org/highfive/backend/curation/service/CurationService.java
+++ src/main/java/org/highfive/backend/curation/service/CurationService.java
@@ -5,7 +5,6 @@ import lombok.RequiredArgsConstructor;
 import org.highfive.backend.content.entity.Content;
 import org.highfive.backend.content.exception.ContentErrorCode;
 import org.highfive.backend.content.repository.ContentRepository;
-import org.highfive.backend.curation.dto.mapper.CurationMapper;
 import org.highfive.backend.curation.dto.request.CreateCurationRequestDto;
 import org.highfive.backend.curation.dto.response.CurationDetailResponseDto;
 import org.highfive.backend.curation.dto.response.MyCurationResponseDto;
@@ -19,13 +18,13 @@ import org.highfive.backend.global.dto.Response;
 import org.highfive.backend.global.exception.BusinessException;
 import org.highfive.backend.user.entity.User;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 
+import static org.highfive.backend.curation.dto.mapper.CurationMapper.*;
+
 @Service
 @RequiredArgsConstructor
-@RequestMapping("/curation")
 public class CurationService {
 
     private final ContentRepository contentRepository;
@@ -36,7 +35,7 @@ public class CurationService {
     public Response<Void> create(final User user, final CreateCurationRequestDto dto) {
 
         final List<Content> contents = getContentsOrThrow(dto.contents());
-        final Curation curation = CurationMapper.toEntity(user, dto);
+        final Curation curation = toEntity(user, dto);
 
         addCurationContents(curation, contents);
 
@@ -48,24 +47,24 @@ public class CurationService {
         final Curation curation = curationQueryRepository.findCurationWithAll(curationId)
                 .orElseThrow(() -> new BusinessException(CurationErrorCode.CURATION_NOT_FOUND));
         final List<CurationDetailResponseDto.ContentDto> contentDtos = mapToContentDtos(curation.getCurationContents());
-        final CurationDetailResponseDto responseDto = CurationMapper.toCurationDetailResponseDto(curation, contentDtos);
+        final CurationDetailResponseDto responseDto = toCurationDetailResponseDto(curation, contentDtos);
 
         return Response.ok(responseDto);
     }
 
     public Response<CursorPageResponse<MyCurationResponseDto>> getMyCurations(final User user, final String cursor, final int size) {
-        List<Curation> curations = curationQueryRepository.findCurationWithUserId(user.getId(), cursor, size);
+        final List<Curation> curations = curationQueryRepository.findCurationWithUserId(user.getId(), cursor, size);
         final boolean hasNext = curations.size() > size;
-        curations = hasNext ? curations.subList(0, size) : curations;
+        final List<Curation> items = hasNext ? curations.subList(0, size) : curations;
+        final String nextCursor = hasNext ? getNextCursor(items) : null;
+        final List<MyCurationResponseDto> myCurationResponseDtos = toMyCurationResponseDtos(items);
 
-        List<MyCurationResponseDto> myCurationResponseDtos = curations.stream()
-                .map(curation -> new MyCurationResponseDto(curation.getId(), curation.getTitle(), curation.getThumbnailUrl()))
-                .toList();
-
-        final String nextCursor = hasNext ? String.valueOf(curations.get(curations.size() - 1).getId()) : null;
-        CursorPageResponse<MyCurationResponseDto> response = new CursorPageResponse<>(myCurationResponseDtos, nextCursor);
+        final CursorPageResponse<MyCurationResponseDto> response = new CursorPageResponse<>(myCurationResponseDtos,  hasNext, nextCursor);
         return Response.ok(response);
+    }
 
+    private String getNextCursor(final List<Curation> curations) {
+        return String.valueOf(curations.get(curations.size() - 1).getId());
     }
 
     private List<CurationDetailResponseDto.ContentDto> mapToContentDtos(final List<CurationContents> curationContentsList) {
