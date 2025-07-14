713f896b3e0f421c601268a647a2b0932ffa43b2
likerhythm <wjdalsdl0412@gmail.com>
2025-07-14 13:14:12 +0900

    feat[#123]: ContentMapper에 dto 매퍼 추가

--- src/main/java/org/highfive/backend/content/dto/mapper/ContentMapper.java
+++ src/main/java/org/highfive/backend/content/dto/mapper/ContentMapper.java
@@ -1,8 +1,11 @@
 package org.highfive.backend.content.dto.mapper;
 
+import java.time.LocalDateTime;
+import org.highfive.backend.content.dto.request.AdminAddContentRequestDto;
 import org.highfive.backend.content.dto.response.ContentDetailResponseDto;
 import org.highfive.backend.content.dto.response.SearchContentResponseDto;
 import org.highfive.backend.content.entity.Content;
+import org.highfive.backend.content.entity.ContentType;
 import org.highfive.backend.global.dto.CursorPageResponse;
 
 import java.util.List;
@@ -25,5 +28,18 @@ public class ContentMapper {
                 .toList();
         return new CursorPageResponse<>(results, String.valueOf(nextCursor));
 
+    }
+
+    public static Content fromAdminAddContentRequestDto(AdminAddContentRequestDto request) {
+        return Content.builder()
+                .title(request.getTitle())
+                .description(request.getDescription())
+                .videoUrl(request.getVideoUrl())
+                .postUrl(request.getPostUrl())
+                .openDate(LocalDateTime.parse(request.getOpenDate()))
+                .runningTime(request.getRunningTime())
+                .totalRound(request.getTotalRound())
+                .contentType(ContentType.valueOf(request.getType()))
+                .build();
     }
 }
