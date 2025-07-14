faaa5eb79cfbeb0213aa5a8bc0bc346955ad5b65
likerhythm <wjdalsdl0412@gmail.com>
2025-07-14 16:55:21 +0900

    feat[#123]: fastapi 서버로부터 벡터를 받아오는 기능 추가

---
+++ src/main/java/org/highfive/backend/global/client/fastapi/dto/response/FastApiVectorFromGenresDto.java
@@ -0,0 +1,13 @@
+package org.highfive.backend.global.client.fastapi.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class FastApiVectorFromGenresDto {
+
+    String vector;
+}
