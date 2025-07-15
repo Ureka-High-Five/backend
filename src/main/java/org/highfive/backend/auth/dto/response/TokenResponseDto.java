9ae71316a8cb4ca67c4721d6d62e56dd0f93e24d
sangyunpark99 <psangyun5108@gmail.com>
2025-07-15 15:36:54 +0900

    chore[#160]: 리프레시 토큰 Null을 넘겨 줌으로써, respons json에서 리프레시 토큰 필드 변수 제외하도록 구현

--- src/main/java/org/highfive/backend/auth/dto/response/TokenResponseDto.java
+++ src/main/java/org/highfive/backend/auth/dto/response/TokenResponseDto.java
@@ -1,5 +1,8 @@
 package org.highfive.backend.auth.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public record TokenResponseDto(
         String accessToken,
         String refreshToken,
