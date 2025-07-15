2e1bae484d9d7ff969e4d55f28dabfd950ffb144
sangyunpark99 <psangyun5108@gmail.com>
2025-07-15 20:51:59 +0900

    feat[#163]: 이미 좋아요를 누른 쇼츠에 해당하는 ErrorCode 구현

---
+++ src/main/java/org/highfive/backend/content/exception/ShortsErrorCode.java
@@ -0,0 +1,32 @@
+package org.highfive.backend.content.exception;
+
+import lombok.RequiredArgsConstructor;
+import org.highfive.backend.global.code.ErrorCode;
+import org.springframework.http.HttpStatus;
+
+@RequiredArgsConstructor
+public enum ShortsErrorCode implements ErrorCode {
+
+    SHORTS_NOT_FOUND(40410, "존재하지 않는 쇼츠입니다.", HttpStatus.NOT_FOUND),
+    SHORTS_ALREADY_LIKED(40901, "이미 좋아요를 누른 쇼츠입니다.", HttpStatus.CONFLICT);
+
+    private final int code;
+    private final String message;
+    private final HttpStatus httpStatus;
+
+
+    @Override
+    public int getCode() {
+        return this.code;
+    }
+
+    @Override
+    public String getMessage() {
+        return this.message;
+    }
+
+    @Override
+    public HttpStatus getHttpStatus() {
+        return this.httpStatus;
+    }
+}
