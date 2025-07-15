8fb02b2e7b17d7c7e895539568cabefbbcad18c8
sangyunpark99 <psangyun5108@gmail.com>
2025-07-15 20:51:15 +0900

    feat[#163]: 쇼츠 좋아요 API 추가

--- src/main/java/org/highfive/backend/content/controller/ShortsController.java
+++ src/main/java/org/highfive/backend/content/controller/ShortsController.java
@@ -1,26 +1,34 @@
 package org.highfive.backend.content.controller;
 
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Positive;
 import lombok.RequiredArgsConstructor;
+import org.highfive.backend.content.dto.request.ShortsLikeRequestDto;
 import org.highfive.backend.content.dto.response.RecommendShortsResponseDto;
 import org.highfive.backend.content.service.ShortsService;
 import org.highfive.backend.global.dto.Response;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.highfive.backend.user.entity.User;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/shorts")
 @RequiredArgsConstructor
 public class ShortsController {
 
     private final ShortsService shortsService;
 
-    @GetMapping("/shorts")
+    @GetMapping
     public Response<RecommendShortsResponseDto> recommendShorts(
             @RequestParam(required = false) @NotEmpty final Long cursor,
             @RequestParam(defaultValue = "5") @Positive final Integer size
     ) {
         return shortsService.recommendShorts(cursor, size);
+    }
+
+    @PostMapping("/like")
+    public Response<Void> like(@AuthenticationPrincipal final User user, @RequestBody @Valid final ShortsLikeRequestDto dto) {
+        return shortsService.like(user, dto);
     }
 }
