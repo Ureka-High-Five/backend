788e0c308cc010739f8c823d208b0743c77fdc4e
JIYEON HWANG <ghkdwldus0807@gmail.com>
2025-07-15 19:19:53 +0900

    fix[#152] : @NotBlank 추가

--- src/main/java/org/highfive/backend/content/dto/request/CreateShortsCommentRequestDto.java
+++ src/main/java/org/highfive/backend/content/dto/request/CreateShortsCommentRequestDto.java
@@ -1,10 +1,12 @@
 package org.highfive.backend.content.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 public record CreateShortsCommentRequestDto(
         @NotNull
+        @NotBlank
         @Size(max = 255)
         String comment,
         @NotNull
