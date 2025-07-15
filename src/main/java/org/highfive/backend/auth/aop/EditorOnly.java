decc49ee9b8a25475e301ed9d4ed9e381471a7fb
sangyunpark99 <psangyun5108@gmail.com>
2025-07-15 15:07:17 +0900

    feat[#158]: 인가 확인에 필요한 Custom Annotation EditorOnly 구현

---
+++ src/main/java/org/highfive/backend/auth/aop/EditorOnly.java
@@ -0,0 +1,9 @@
+package org.highfive.backend.auth.aop;
+
+import java.lang.annotation.*;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface EditorOnly {
+}
