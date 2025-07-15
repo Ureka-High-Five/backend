b77627eddba82b23b193d100d7fdfb37da616f49
sangyunpark99 <psangyun5108@gmail.com>
2025-07-15 20:51:59 +0900

    feat[#163]: 이미 좋아요를 누른 쇼츠에 해당하는 ErrorCode 구현

--- src/main/java/org/highfive/backend/content/exception/ShortsErrorCode.java
+++ src/main/java/org/highfive/backend/content/exception/ShortsErrorCode.java
@@ -7,10 +7,10 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 public enum ShortsErrorCode implements ErrorCode {
 
+    SHORTS_NOT_FOUND(40410, "존재하지 않는 쇼츠입니다.", HttpStatus.NOT_FOUND),
+    SHORTS_ALREADY_LIKED(40901, "이미 좋아요를 누른 쇼츠입니다.", HttpStatus.CONFLICT),
 
-    SHORTS_COMMENT_CONTAINS_PROHIBITED_WORD(42202, "Shorts 댓글에 부적절한 단어가 포함되어있습니다.", HttpStatus.UNPROCESSABLE_ENTITY),
-    SHORTS_NOT_FOUND(40410, "존재하지 않는 Shorts입니다.", HttpStatus.NOT_FOUND),
-    ;
+    SHORTS_COMMENT_CONTAINS_PROHIBITED_WORD(42202, "Shorts 댓글에 부적절한 단어가 포함되어있습니다.", HttpStatus.UNPROCESSABLE_ENTITY);
 
     private final int code;
     private final String message;
