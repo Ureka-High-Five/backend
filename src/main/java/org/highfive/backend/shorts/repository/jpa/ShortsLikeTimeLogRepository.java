f563aa4b6fd4c48dee2835bc51a0a379bfbe3ab6
sangyunpark99 <psangyun5108@gmail.com>
2025-07-16 11:09:42 +0900

    Feat[#171]: 유저 아이디, 쇼츠 아이디 값을 통해서 ShortsLikeTimeLog 객체를 조회하는 메서드 구현

--- src/main/java/org/highfive/backend/shorts/repository/jpa/ShortsLikeTimeLogRepository.java
+++ src/main/java/org/highfive/backend/shorts/repository/jpa/ShortsLikeTimeLogRepository.java
@@ -3,7 +3,11 @@ package org.highfive.backend.shorts.repository.jpa;
 import org.highfive.backend.shorts.entity.ShortsLikeTimeLog;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ShortsLikeTimeLogRepository extends JpaRepository<ShortsLikeTimeLog, Long> {
 
     boolean existsByUserIdAndShortsId(Long userId, Long shortsId);
+
+    Optional<ShortsLikeTimeLog> findByUserIdAndShortsId(Long userId, Long shortsId);
 }
