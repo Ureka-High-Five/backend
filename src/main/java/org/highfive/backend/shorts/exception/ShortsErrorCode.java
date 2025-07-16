193b04d08becb3c1d04bd901eb1c8a5ed780425c
sangyunpark99 <psangyun5108@gmail.com>
2025-07-16 11:08:52 +0900

    Feat[#171]: 좋아요 취소 에러 코드 구현 (로그인한 유저가 누른 좋아요가 존재하지 않는 경우)

--- src/main/java/org/highfive/backend/shorts/exception/ShortsErrorCode.java
+++ src/main/java/org/highfive/backend/shorts/exception/ShortsErrorCode.java
@@ -9,7 +9,7 @@ public enum ShortsErrorCode implements ErrorCode {
 
     SHORTS_NOT_FOUND(40410, "존재하지 않는 쇼츠입니다.", HttpStatus.NOT_FOUND),
     SHORTS_ALREADY_LIKED(40901, "이미 좋아요를 누른 쇼츠입니다.", HttpStatus.CONFLICT),
-
+    SHORTS_LIKED_NOT_FOUND(40411, "해당 유저의 쇼츠 좋아요가 존재하지 않습니다.", HttpStatus.NOT_FOUND),
     SHORTS_COMMENT_CONTAINS_PROHIBITED_WORD(42202, "Shorts 댓글에 부적절한 단어가 포함되어있습니다.", HttpStatus.UNPROCESSABLE_ENTITY);
 
     private final int code;
