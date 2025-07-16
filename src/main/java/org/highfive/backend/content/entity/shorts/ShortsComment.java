9896561a469c696333f12e812c3e809dee42aa5f
sangyunpark99 <psangyun5108@gmail.com>
2025-07-16 09:45:02 +0900

    chore[#172]: shorts, shortsComment 엔티티에 BaseEntity 상속

--- src/main/java/org/highfive/backend/content/entity/shorts/ShortsComment.java
+++ src/main/java/org/highfive/backend/content/entity/shorts/ShortsComment.java
@@ -5,6 +5,7 @@ import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.NoArgsConstructor;
+import org.highfive.backend.global.entity.BaseEntity;
 import org.highfive.backend.user.entity.User;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
@@ -16,7 +17,7 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 @EntityListeners(AuditingEntityListener.class)
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class ShortsComment {
+public class ShortsComment extends BaseEntity {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
