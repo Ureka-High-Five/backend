9f5e7394ae73d548d6cd3cfcac4349996d0ee712
sangyunpark99 <psangyun5108@gmail.com>
2025-07-16 14:05:30 +0900

    feat[#181]: 쇼츠 좋아요 시간대별 조회 API 추가

--- src/main/java/org/highfive/backend/shorts/controller/ShortsController.java
+++ src/main/java/org/highfive/backend/shorts/controller/ShortsController.java
@@ -4,19 +4,17 @@ import jakarta.validation.Valid;
 import jakarta.validation.constraints.Positive;
 import lombok.RequiredArgsConstructor;
 import org.highfive.backend.shorts.dto.request.ShortsDislikeRequestDto;
-import org.highfive.backend.shorts.dto.request.ShortsLikeRequestDto;
+import org.highfive.backend.shorts.dto.request.ShortsLikeCreateRequestDto;
 import org.highfive.backend.shorts.dto.response.RecommendShortsResponseDto;
 import org.highfive.backend.shorts.dto.request.CreateShortsCommentRequestDto;
+import org.highfive.backend.shorts.dto.response.ShortsLikeTimeResponseDto;
 import org.highfive.backend.shorts.service.ShortsService;
 import org.highfive.backend.global.dto.Response;
 import org.highfive.backend.user.entity.User;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -42,12 +40,20 @@ public class ShortsController {
     }
 
     @PostMapping("/like")
-    public Response<Void> like(@AuthenticationPrincipal final User user, @RequestBody @Valid final ShortsLikeRequestDto dto) {
+    public Response<Void> like(@AuthenticationPrincipal final User user,
+                               @RequestBody @Valid final ShortsLikeCreateRequestDto dto) {
         return shortsService.like(user, dto);
     }
 
     @PostMapping("/dislike")
-    public Response<Void> dislike(@AuthenticationPrincipal final User user, @RequestBody @Valid final ShortsDislikeRequestDto dto) {
+    public Response<Void> dislike(@AuthenticationPrincipal final User user,
+                                  @RequestBody @Valid final ShortsDislikeRequestDto dto) {
         return shortsService.dislike(user, dto);
+    }
+
+    @GetMapping("/like")
+    public Response<ShortsLikeTimeResponseDto> getShortsLike(@RequestParam long shortsId,
+                                                             @RequestParam @Positive int duration) {
+        return shortsService.getShortsLike(shortsId, duration);
     }
 }
