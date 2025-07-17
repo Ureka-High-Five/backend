b4bf4c5f8a0375cf6e2c07346b0bb71263e5faeb
likerhythm <wjdalsdl0412@gmail.com>
2025-07-17 10:24:48 +0900

    fix[#196]: cors 문제 해결

--- src/main/java/org/highfive/backend/global/config/SecurityConfig.java
+++ src/main/java/org/highfive/backend/global/config/SecurityConfig.java
@@ -61,7 +61,8 @@ public class SecurityConfig {
                 "http://localhost:5173",
                 "https://lead-me.site",
                 "https://www.lead-me.site",
-                "http://localhost:8080"
+                "http://localhost:8080",
+                "https://api.lead-me.site"
         ));
 
         config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
