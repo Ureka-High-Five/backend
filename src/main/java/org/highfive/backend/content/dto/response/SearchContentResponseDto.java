07b6010396dd49b9c382bcfb55dba6f6e76d36fc
likerhythm <wjdalsdl0412@gmail.com>
2025-07-17 16:48:05 +0900

    refactor[#203]: 필드 이름 thumbnailUrl로 변경

--- src/main/java/org/highfive/backend/content/dto/response/SearchContentResponseDto.java
+++ src/main/java/org/highfive/backend/content/dto/response/SearchContentResponseDto.java
@@ -2,7 +2,7 @@ package org.highfive.backend.content.dto.response;
 
 public record SearchContentResponseDto(
         long contentId,
-        String posterUrl,
+        String thumbnailUrl,
         String title,
         int openYear
 ) {
