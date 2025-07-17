0f807cd12c8c533675fec895ff54b3d1ced7ab98
likerhythm <wjdalsdl0412@gmail.com>
2025-07-17 17:45:00 +0900

    feat[#140]: 컨트롤러 계층 구현

--- src/main/java/org/highfive/backend/content/controller/AdminContentController.java
+++ src/main/java/org/highfive/backend/content/controller/AdminContentController.java
@@ -10,26 +10,35 @@ import org.highfive.backend.content.service.AdminContentService;
 import org.highfive.backend.global.dto.Response;
 import org.highfive.backend.user.entity.User;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/content")
 @RequiredArgsConstructor
 public class AdminContentController {
 
     private final AdminContentService adminContentService;
 
-    @PostMapping("/content")
+    @PostMapping
     public Response<AdminAddContentResponseDto> adminAddContent(@Valid @RequestBody final AdminAddContentRequestDto request,
                                                                 @AuthenticationPrincipal final User user) {
         return adminContentService.addContent(request, user);
     }
 
-    @PatchMapping("/content")
+    @PatchMapping
     public Response<AdminUpdateContentResponseDto> adminUpdateContent(@Valid @RequestBody final AdminUpdateContentRequestDto request,
                                                                       @AuthenticationPrincipal final User user) {
         return adminContentService.updateContent(request, user);
+    }
+
+    @DeleteMapping("/{contentId}")
+    public Response<Void> adminDeleteContent(@PathVariable Long contentId) {
+        return adminContentService.deleteContent(contentId);
     }
 }
