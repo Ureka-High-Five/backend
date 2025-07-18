4137a35da66c54974a9d705b408edc68b76e15dc
JIYEON HWANG <ghkdwldus0807@gmail.com>
2025-07-19 01:24:07 +0900

    refactor[#219] : AdiminUserController에서 ReqeustParam 대신 Body를 받도록 수정

--- src/main/java/org/highfive/backend/user/controller/AdminUserController.java
+++ src/main/java/org/highfive/backend/user/controller/AdminUserController.java
@@ -4,9 +4,14 @@ import lombok.RequiredArgsConstructor;
 import org.highfive.backend.auth.aop.AdminOnly;
 import org.highfive.backend.global.dto.CursorPageResponse;
 import org.highfive.backend.global.dto.Response;
+import org.highfive.backend.user.dto.request.UpdateUserRoleRequestDto;
 import org.highfive.backend.user.dto.response.GetAllUserResponseDto;
+import org.highfive.backend.user.entity.User;
 import org.highfive.backend.user.service.AdminUserService;
+import org.springframework.security.core.AuthenticatedPrincipal;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -28,8 +33,8 @@ public class AdminUserController {
     @AdminOnly
     @GetMapping("/user/role")
     public Response<Void> updateUserRole(
-            @RequestParam Long userId
+            @RequestBody UpdateUserRoleRequestDto request
     ){
-        return adminUserService.updateUserRole(userId);
+        return adminUserService.updateUserRole(request);
     }
 }
