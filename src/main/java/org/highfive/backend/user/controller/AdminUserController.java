8d9f7e274345eae678ac5cb6caee2a98eec3a514
JIYEON HWANG <ghkdwldus0807@gmail.com>
2025-07-19 02:07:57 +0900

    fix[#219] : 사용자 권한 변경 api의 HTTP 메서드를 Patch로 변경

--- src/main/java/org/highfive/backend/user/controller/AdminUserController.java
+++ src/main/java/org/highfive/backend/user/controller/AdminUserController.java
@@ -6,11 +6,9 @@ import org.highfive.backend.global.dto.CursorPageResponse;
 import org.highfive.backend.global.dto.Response;
 import org.highfive.backend.user.dto.request.UpdateUserRoleRequestDto;
 import org.highfive.backend.user.dto.response.GetAllUserResponseDto;
-import org.highfive.backend.user.entity.User;
 import org.highfive.backend.user.service.AdminUserService;
-import org.springframework.security.core.AuthenticatedPrincipal;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -31,7 +29,7 @@ public class AdminUserController {
     }
 
     @AdminOnly
-    @GetMapping("/user/role")
+    @PatchMapping("/user/role")
     public Response<Void> updateUserRole(
             @RequestBody UpdateUserRoleRequestDto request
     ){
