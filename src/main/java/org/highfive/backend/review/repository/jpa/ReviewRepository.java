2fb67a5769051cf298e45fbe49214c28330c26c6
sangyunpark99 <psangyun5108@gmail.com>
2025-07-19 01:19:29 +0900

    feat[#229]: 사용자가 남긴 리뷰의 컨텐츠를 조회하는 쿼리 구현

--- src/main/java/org/highfive/backend/review/repository/jpa/ReviewRepository.java
+++ src/main/java/org/highfive/backend/review/repository/jpa/ReviewRepository.java
@@ -1,11 +1,35 @@
 package org.highfive.backend.review.repository.jpa;
 
-import org.highfive.backend.review.repository.querydsl.ReviewQueryRepository;
 import org.highfive.backend.review.entity.Review;
+import org.highfive.backend.review.repository.querydsl.ReviewQueryRepository;
+import org.highfive.backend.user.dto.response.RatedContentResponseDto;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface ReviewRepository extends JpaRepository<Review, Long>, ReviewQueryRepository {
     Optional<Review> findByUserIdAndContentId(Long userId, Long contentId);
+
+    @Query(value = """
+                SELECT
+                    r.id,
+                    c.thumbnailUrl,
+                    c.title,
+                    r.reviewText,
+                    r.rating
+                FROM Review r
+                JOIN r.content c
+                WHERE r.user.id = :userId
+                  AND (:cursor IS NULL OR r.id < :cursor)
+                ORDER BY r.id DESC
+            """)
+    List<RatedContentResponseDto> findByUser(
+            @Param("userId") Long userId,
+            @Param("cursor") String cursor,
+            Pageable pageable
+    );
 }
