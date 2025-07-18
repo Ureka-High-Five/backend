857e4dc4e79eec7134f7ea28e5e82067dcfb2d2d
likerhythm <wjdalsdl0412@gmail.com>
2025-07-18 13:43:28 +0900

    feat[#213]: GetAllUserResponseDto 매퍼 추가

--- src/main/java/org/highfive/backend/user/dto/mapper/UserMapper.java
+++ src/main/java/org/highfive/backend/user/dto/mapper/UserMapper.java
@@ -1,6 +1,7 @@
 package org.highfive.backend.user.dto.mapper;
 
 import org.highfive.backend.auth.client.dto.response.KakaoUserResponseDto;
+import org.highfive.backend.user.dto.response.GetAllUserResponseDto;
 import org.highfive.backend.user.dto.response.UserInfoResponseDto;
 import org.highfive.backend.user.entity.UserRole;
 import org.highfive.backend.user.entity.User;
@@ -20,5 +21,9 @@ public class UserMapper {
 
     public static UserInfoResponseDto from(final User user) {
         return new UserInfoResponseDto(user.getId(), user.getName(), user.getEmail(), user.getUserRole().name(), user.getProfileUrl());
+    }
+
+    public static GetAllUserResponseDto toGetAllUserResponseDto(User user) {
+        return new GetAllUserResponseDto(user.getProfileUrl(), user.getName(), user.getEmail(), user.getUserRole().toString());
     }
 }
