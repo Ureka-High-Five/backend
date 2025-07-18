f33d8e48464419aa320083b33c3b6d6e9ebb5460
likerhythm <wjdalsdl0412@gmail.com>
2025-07-18 13:25:54 +0900

    refactor[#213]: CursorPageResponse를 반환하도록 수정

--- src/main/java/org/highfive/backend/user/controller/AdminUserController.java
+++ src/main/java/org/highfive/backend/user/controller/AdminUserController.java
@@ -2,6 +2,7 @@ package org.highfive.backend.user.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.highfive.backend.auth.aop.AdminOnly;
+import org.highfive.backend.global.dto.CursorPageResponse;
 import org.highfive.backend.global.dto.Response;
 import org.highfive.backend.user.dto.response.GetAllUserResponseDto;
 import org.highfive.backend.user.service.AdminUserService;
@@ -17,7 +18,7 @@ public class AdminUserController {
 
     @AdminOnly
     @GetMapping("/user")
-    public Response<GetAllUserResponseDto> getAllUser(
+    public Response<CursorPageResponse<GetAllUserResponseDto>> getAllUser(
             @RequestParam(required = false) Long cursor,
             @RequestParam(required = false, defaultValue = "10") Integer size
     ) {
