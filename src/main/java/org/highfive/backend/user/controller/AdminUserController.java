9e8473967a4b180f24a5f98ac582c17436959c3e
likerhythm <wjdalsdl0412@gmail.com>
2025-07-18 13:24:40 +0900

    feat[#213]: 컨트롤러 계층 구현

---
+++ src/main/java/org/highfive/backend/user/controller/AdminUserController.java
@@ -0,0 +1,26 @@
+package org.highfive.backend.user.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.highfive.backend.auth.aop.AdminOnly;
+import org.highfive.backend.global.dto.Response;
+import org.highfive.backend.user.dto.response.GetAllUserResponseDto;
+import org.highfive.backend.user.service.AdminUserService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class AdminUserController {
+
+    private final AdminUserService adminUserService;
+
+    @AdminOnly
+    @GetMapping("/user")
+    public Response<GetAllUserResponseDto> getAllUser(
+            @RequestParam(required = false) Long cursor,
+            @RequestParam(required = false, defaultValue = "10") Integer size
+    ) {
+        return adminUserService.getAllUser(cursor, size);
+    }
+}
