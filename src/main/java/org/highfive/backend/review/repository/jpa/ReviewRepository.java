592ab18d6771ff88675f19850cb3d524980ccc88
sangyunpark99 <psangyun5108@gmail.com>
2025-07-19 01:28:47 +0900

    refactor[#229]: 불필요한 @Param 어노테이션 제거

--- src/main/java/org/highfive/backend/review/repository/jpa/ReviewRepository.java
+++ src/main/java/org/highfive/backend/review/repository/jpa/ReviewRepository.java
@@ -6,7 +6,6 @@ import org.highfive.backend.user.dto.response.RatedContentResponseDto;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
@@ -28,8 +27,8 @@ public interface ReviewRepository extends JpaRepository<Review, Long>, ReviewQue
                 ORDER BY r.id DESC
             """)
     List<RatedContentResponseDto> findByUser(
-            @Param("userId") Long userId,
-            @Param("cursor") String cursor,
+            Long userId,
+            String cursor,
             Pageable pageable
     );
 }
