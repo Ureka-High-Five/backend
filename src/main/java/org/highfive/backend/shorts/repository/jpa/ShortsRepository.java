e3102bdfba99ef191f1229966790f55b94eba2f9
JIYEON HWANG <ghkdwldus0807@gmail.com>
2025-07-18 13:22:45 +0900

    feat[#207] : ShortsRepository에 contentId 기반 랜덤 쇼츠 조회 메서드 추가

--- src/main/java/org/highfive/backend/shorts/repository/jpa/ShortsRepository.java
+++ src/main/java/org/highfive/backend/shorts/repository/jpa/ShortsRepository.java
@@ -1,5 +1,7 @@
 package org.highfive.backend.shorts.repository.jpa;
 
+import io.lettuce.core.dynamic.annotation.Param;
+import java.util.Optional;
 import org.highfive.backend.shorts.entity.Shorts;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
@@ -14,4 +16,9 @@ public interface ShortsRepository extends JpaRepository<Shorts, Long> {
     @Modifying
     @Query("update Shorts s set s.likeCount = s.likeCount - 1 where s.id = :id and s.likeCount > 0")
     void decreaseLike(Long id);
+
+    @Query("SELECT s FROM Shorts s WHERE s.content.id = :contentId ORDER BY function('RAND')")
+    Optional<Shorts> findRandomByContentId(@Param("contentId") Long contentId);
+
+
 }
