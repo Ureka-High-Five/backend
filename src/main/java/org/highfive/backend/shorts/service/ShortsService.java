d9f2cb0591bcef5ee2e050ee84825e34b36e15f3
likerhythm <wjdalsdl0412@gmail.com>
2025-07-18 15:26:22 +0900

    fix[#211]: 결과를 size만큼 자르는 로직 추가

--- src/main/java/org/highfive/backend/shorts/service/ShortsService.java
+++ src/main/java/org/highfive/backend/shorts/service/ShortsService.java
@@ -72,6 +72,7 @@ public class ShortsService {
         List<ShortsResponseDto> result = getRecommendResult(user, recommend);
         boolean hasNext = result.size() > size;
         Long nextCursor = hasNext ? recommend.getLast().getId() : null;
+        result = new ArrayList<>(result.subList(0, Math.min(5, result.size())));
         CursorPageResponse<ShortsResponseDto> response = new CursorPageResponse<>(result, hasNext, String.valueOf(nextCursor));
         return Response.ok(new RecommendShortsResponseDto(
                 response,
