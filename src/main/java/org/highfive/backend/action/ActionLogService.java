4e4865d851cc0dbfc21f3a40ca5a3f04da6a212e
likerhythm <wjdalsdl0412@gmail.com>
2025-07-19 16:01:32 +0900

    chore[#234]: 테스트 용으로 직접 저장이 아닌 로그 찍기

--- src/main/java/org/highfive/backend/action/ActionLogService.java
+++ src/main/java/org/highfive/backend/action/ActionLogService.java
@@ -1,8 +1,10 @@
 package org.highfive.backend.action;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class ActionLogService {
@@ -10,6 +12,7 @@ public class ActionLogService {
     private final ActionLogRepository actionLogRepository;
 
     public void saveLog(ActionLog actionLog) {
-        actionLogRepository.save(actionLog);
+        log.info("ActionLog = {}", actionLog.toString());
+//        actionLogRepository.save(actionLog);
     }
 }
