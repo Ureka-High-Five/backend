74bf71418f04b87d384b17d7ee73ddd6dc1021d0
sangyunpark99 <psangyun5108@gmail.com>
2025-07-19 23:59:29 +0900

    test[#233]: @Import 어노테이션 삭제

--- src/test/java/org/highfive/backend/BackendApplicationTests.java
+++ src/test/java/org/highfive/backend/BackendApplicationTests.java
@@ -1,13 +1,10 @@
 package org.highfive.backend;
 
-import org.highfive.backend.config.RabbitMQMockConfig;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
 
 @ActiveProfiles("test")
-@Import(RabbitMQMockConfig.class)
 @SpringBootTest
 class BackendApplicationTests {
 
