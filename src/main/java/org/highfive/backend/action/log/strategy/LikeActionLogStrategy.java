53574038333b6a640d89172f40368b70760ad04e
likerhythm <wjdalsdl0412@gmail.com>
2025-07-20 13:48:00 +0900

    feat[#234]: 좋아요 행동 전략 구현

---
+++ src/main/java/org/highfive/backend/action/log/strategy/LikeActionLogStrategy.java
@@ -0,0 +1,49 @@
+package org.highfive.backend.action.log.strategy;
+
+import lombok.RequiredArgsConstructor;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.highfive.backend.action.Action;
+import org.highfive.backend.action.log.ActionLog;
+import org.highfive.backend.global.code.GlobalErrorCode;
+import org.highfive.backend.global.exception.BusinessException;
+import org.highfive.backend.shorts.dto.request.ShortsLikeCreateRequestDto;
+import org.highfive.backend.shorts.entity.Shorts;
+import org.highfive.backend.shorts.exception.ShortsErrorCode;
+import org.highfive.backend.shorts.repository.jpa.ShortsRepository;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class LikeActionLogStrategy implements ActionLogStrategy {
+
+    private final ShortsRepository shortsRepository;
+
+    @Override
+    public ActionLog createLog(ProceedingJoinPoint joinPoint, long userId, long timestamp) {
+        long shortsId = extractShortsLikeCreateRequestDto(joinPoint);
+        long contentId = getContentIdByShortsId(shortsId);
+        return ActionLog.builder()
+                .userId(userId)
+                .contentId(contentId)
+                .action(Action.LIKE)
+                .value(1)
+                .timestamp(timestamp)
+                .build();
+    }
+
+    private long extractShortsLikeCreateRequestDto(ProceedingJoinPoint joinPoint) {
+        Object[] args = joinPoint.getArgs();
+        for (Object arg : args) {
+            if (arg instanceof ShortsLikeCreateRequestDto dto) {
+                return dto.shortsId();
+            }
+        }
+        throw new BusinessException(GlobalErrorCode.BAD_REQUEST);
+    }
+
+
+    private long getContentIdByShortsId(long shortsId) {
+        Shorts shorts = shortsRepository.findById(shortsId).orElseThrow(() -> new BusinessException(ShortsErrorCode.SHORTS_NOT_FOUND));
+        return shorts.getContent().getId();
+    }
+}
