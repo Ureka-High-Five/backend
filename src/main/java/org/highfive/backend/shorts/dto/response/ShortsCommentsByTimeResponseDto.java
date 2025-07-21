e47b2c30a7cbcd534ae9eeec83265fcf257bd8d0
sangyunpark99 <psangyun5108@gmail.com>
2025-07-21 10:55:45 +0900

    test[#245]: username에서 userName으로 변경

--- src/main/java/org/highfive/backend/shorts/dto/response/ShortsCommentsByTimeResponseDto.java
+++ src/main/java/org/highfive/backend/shorts/dto/response/ShortsCommentsByTimeResponseDto.java
@@ -2,7 +2,7 @@ package org.highfive.backend.shorts.dto.response;
 
 public record ShortsCommentsByTimeResponseDto(
        Long time,
-       String username,
+       String userName,
        String profileUrl,
        String comment,
        Long userId
