7f70ccf13a2c0edc5e006f6b575a3a14140681a1
sangyunpark99 <psangyun5108@gmail.com>
2025-07-21 09:06:00 +0900

    feat[#240]: 메시지 재처리 config 구현

---
+++ src/main/java/org/highfive/backend/global/config/MessageRetryConfig.java
@@ -0,0 +1,9 @@
+package org.highfive.backend.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.retry.annotation.EnableRetry;
+
+@EnableRetry
+@Configuration
+public class MessageRetryConfig {
+}
