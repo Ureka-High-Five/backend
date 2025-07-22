6b52fc6818542fdb3a3fcc1d891abe4606633ade
sangyunpark99 <psangyun5108@gmail.com>
2025-07-22 21:08:41 +0900

    chore[#259]: 사용하지 않는 import 구문 제거

--- src/main/java/org/highfive/backend/shorts/entity/Shorts.java
+++ src/main/java/org/highfive/backend/shorts/entity/Shorts.java
@@ -1,25 +1,12 @@
 package org.highfive.backend.shorts.entity;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
-import jakarta.persistence.CascadeType;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
-import java.util.ArrayList;
-import java.util.List;
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import jakarta.persistence.*;
+import lombok.*;
 import org.highfive.backend.content.entity.Content;
 import org.highfive.backend.global.entity.BaseEntity;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter
