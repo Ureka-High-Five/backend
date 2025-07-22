5a5f5db19c77318e6b860545740d758ccd0f5c89
sangyunpark99 <psangyun5108@gmail.com>
2025-07-22 09:38:07 +0900

    feat[#244]: redis의 최신화된 uservector값 DB랑 동기화

--- src/main/java/org/highfive/backend/content/service/HomeContentService.java
+++ src/main/java/org/highfive/backend/content/service/HomeContentService.java
@@ -1,9 +1,5 @@
 package org.highfive.backend.content.service;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.highfive.backend.content.dto.response.HomeContentsResponseDto;
@@ -21,8 +17,14 @@ import org.highfive.backend.global.dto.Response;
 import org.highfive.backend.global.exception.BusinessException;
 import org.highfive.backend.user.entity.User;
 import org.highfive.backend.user.entity.preference.PreferMetaInfoRepository;
+import org.highfive.backend.user.repository.jpa.UserRepository;
 import org.highfive.backend.user.repository.redis.UserRedisRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Slf4j
 @Service
@@ -33,6 +35,7 @@ public class HomeContentService {
     private final UserRedisRepository userRedisRepository;
     private final ContentRepository contentRepository;
     private final PreferMetaInfoRepository preferMetaInfoRepository;
+    private final UserRepository userRepository;
 
     public Response<HomeContentsResponseDto> getHomeContents(final User user) {
         final MainRecommendDto mainRecommend = recommendMainContentsByUser(user);
@@ -41,37 +44,37 @@ public class HomeContentService {
 
         // todo 사용자가 선호하는 장르 기반 큐레이션 조회(1차 MVP 이후)
 
-        final HomeContentsResponseDto result = new HomeContentsResponseDto(mainRecommend, personalRecommends,
-                genreRecommends, null);
+        final HomeContentsResponseDto result = new HomeContentsResponseDto(mainRecommend, personalRecommends, genreRecommends, null);
         return new Response<>(SuccessCode.OK.getCode(), result, null);
     }
 
     private MainRecommendDto recommendMainContentsByUser(final User user) {
-        final String userVector = userRedisRepository.getUserVector(user.getId());
-        log.info("user vector: {}", userVector);
-        final List<FastApiRecommendResponseDto> contentsByUserVector = fastApiVectorRecommend(user.getEmbedding(), 1);
+        final List<FastApiRecommendResponseDto> contentsByUserVector = recommendContentsByVector(user, 1);
         final Content content = contentRepository.findById(contentsByUserVector.getFirst().id())
                 .orElseThrow(() -> new BusinessException(ContentErrorCode.CONTENT_NOT_FOUND));
         final List<String> genres = getGenres(content);
-        return new MainRecommendDto(content.getId(), content.getPostUrl(), content.getDescription(), genres,
-                content.getTitle());
+        return new MainRecommendDto(content.getId(), content.getPostUrl(), content.getDescription(), genres, content.getTitle());
     }
 
     private List<PersonalRecommendDto> recommendContentsByUser(final User user, final int count) {
+        return recommendContentsByVector(user, count).stream()
+                .map(dto -> {
+                    Content content = getContentById(dto.id());
+                    return new PersonalRecommendDto(content.getId(), content.getThumbnailUrl());
+                })
+                .toList();
+    }
+
+    private Content getContentById(long contentId) {
+        return contentRepository.findById(contentId)
+                .orElseThrow(() -> new BusinessException(ContentErrorCode.CONTENT_NOT_FOUND));
+    }
+
+    private List<FastApiRecommendResponseDto> recommendContentsByVector(final User user, final int count) {
         final String userVector = userRedisRepository.getUserVector(user.getId());
-        log.info("user vector: {}", userVector);
-        final List<FastApiRecommendResponseDto> contentsByUserVector = fastApiVectorRecommend(user.getEmbedding(),
-                count);
-        final List<PersonalRecommendDto> result = new ArrayList<>();
-        for (FastApiRecommendResponseDto dto : contentsByUserVector) {
-            long contentId = dto.id();
-            Content content = contentRepository.findById(contentId)
-                    .orElseThrow(() -> new BusinessException(ContentErrorCode.CONTENT_NOT_FOUND));
-            String thumbnailUrl = content.getThumbnailUrl();
-            PersonalRecommendDto resultDto = new PersonalRecommendDto(contentId, thumbnailUrl);
-            result.add(resultDto);
-        }
-        return result;
+        log.info("userId={}, user vector={}", user.getId(), userVector);
+        userRepository.upsertUserVector(user.getId(), userVector);
+        return fastApiVectorRecommend(userVector, count);
     }
 
     private Map<String, List<GenreContentDto>> recommendContentsByUserGenre(final User user, final int count) {
@@ -79,16 +82,13 @@ public class HomeContentService {
         final Map<String, List<GenreContentDto>> result = new HashMap<>();
         for (String genre : preferGenresByUser) {
             List<TopContentsByGenreDto> topContentsByGenre = contentRepository.findTopContentsByGenre(genre, 5);
-            result.put(genre,
-                    topContentsByGenre.stream().map(tc -> new GenreContentDto(tc.contentId(), tc.thumbnailUrl()))
-                            .toList());
+            result.put(genre, topContentsByGenre.stream().map(tc -> new GenreContentDto(tc.contentId(), tc.thumbnailUrl())).toList());
         }
         return result;
     }
 
     private List<String> getGenres(final Content content) {
-        final List<Map<String, Object>> contentGenresByContentIds = contentRepository.findContentGenresByContentIds(
-                List.of(content.getId()));
+        final List<Map<String, Object>> contentGenresByContentIds = contentRepository.findContentGenresByContentIds(List.of(content.getId()));
         final List<String> genres = new ArrayList<>();
         for (Map<String, Object> genreInfo : contentGenresByContentIds) {
             String genreName = (String) genreInfo.get("genreName");
