ab6ffb2645be3f4d860e441a99f62c0a797d46d8
sangyunpark99 <psangyun5108@gmail.com>
2025-07-22 10:24:15 +0900

    feat[#244]: 벡터값으로 변환하는 로직 추가 구현

--- src/main/java/org/highfive/backend/user/repository/jpa/UserRepository.java
+++ src/main/java/org/highfive/backend/user/repository/jpa/UserRepository.java
@@ -1,9 +1,9 @@
 package org.highfive.backend.user.repository.jpa;
 
-import java.util.List;
-import org.highfive.backend.user.dto.response.SearchUserResponseDto;
 import org.highfive.backend.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 
@@ -12,11 +12,11 @@ public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByKakaoUserId(String kakaoUserId);
 
     @Modifying
-    @Query(value =
-            """
+    @Query(value = """
                 INSERT INTO users_vector (user_id, embedding, created_at, updated_at)
-                VALUES (:userId, :embedding, now(), now())  
-                ON CONFLICT (user_id) DO UPDATE SET embedding = :embedding, updated_at = now()
+                VALUES (:userId, CAST(:embedding AS vector), now(), now())
+                ON CONFLICT (user_id) DO UPDATE 
+                SET embedding = CAST(:embedding AS vector), updated_at = now()
             """, nativeQuery = true)
     void upsertUserVector(Long userId, String embedding);
 }
