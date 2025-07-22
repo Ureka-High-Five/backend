3c019259608c7c6e6ad6676707611c49dbae87a1
sangyunpark99 <psangyun5108@gmail.com>
2025-07-22 09:39:50 +0900

    feat[#244]: userId 값을 기반으로 벡터가 없는경우 insert하고, userId 값ㅇ르 기반으로 벡터가 존재하는 경우 update해주는 쿼리 구현

--- src/main/java/org/highfive/backend/user/repository/jpa/UserRepository.java
+++ src/main/java/org/highfive/backend/user/repository/jpa/UserRepository.java
@@ -1,13 +1,22 @@
 package org.highfive.backend.user.repository.jpa;
 
 import java.util.List;
-import java.util.Optional;
+import org.highfive.backend.user.dto.response.SearchUserResponseDto;
 import org.highfive.backend.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findByKakaoUserId(String kakaoUserId);
 
-    List<User> findByNameContaining(String username);
+    @Modifying
+    @Query(value =
+            """
+                INSERT INTO users_vector (user_id, embedding, created_at, updated_at)
+                VALUES (:userId, :embedding, now(), now())  
+                ON CONFLICT (user_id) DO UPDATE SET embedding = :embedding, updated_at = now()
+            """, nativeQuery = true)
+    void upsertUserVector(Long userId, String embedding);
 }
