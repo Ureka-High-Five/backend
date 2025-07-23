154e5edae7dc3ba009d277a2fc136f54d8a50077
sangyunpark99 <psangyun5108@gmail.com>
2025-07-23 13:17:16 +0900

    feat[#270]: presignedUrl을 반환하는 비즈니스 로직 구현

---
+++ src/main/java/org/highfive/backend/infra/s3/service/S3Service.java
@@ -0,0 +1,45 @@
+package org.highfive.backend.infra.s3.service;
+
+import lombok.RequiredArgsConstructor;
+import org.highfive.backend.global.dto.Response;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+import software.amazon.awssdk.services.s3.presigner.S3Presigner;
+import software.amazon.awssdk.services.s3.presigner.model.PutObjectPresignRequest;
+
+import java.net.URL;
+import java.time.Duration;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class S3Service {
+
+    private final int DURATION = 5;
+    private final String FOLDDER = "curation_thumbnail";
+
+    private final S3Presigner s3Presigner;
+
+    @Value("${cloud.aws.s3.bucket}")
+    private String bucket;
+
+    public Response<URL> generatePresignedUrl() {
+        final String key = FOLDDER + "/" + UUID.randomUUID();
+
+        PutObjectRequest putObjectRequest = PutObjectRequest.builder()
+                .bucket(bucket)
+                .key(key)
+                .contentType("image/jpeg")
+                .build();
+
+        PutObjectPresignRequest presignRequest = PutObjectPresignRequest.builder()
+                .putObjectRequest(putObjectRequest)
+                .signatureDuration(Duration.ofMinutes(DURATION))
+                .build();
+
+        final URL url = s3Presigner.presignPutObject(presignRequest).url();
+
+        return Response.ok(url);
+    }
+}
