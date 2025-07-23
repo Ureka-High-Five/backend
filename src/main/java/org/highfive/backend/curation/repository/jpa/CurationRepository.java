d7be40df024a7b1865fdb392c30bd4ff5fb7e9bf
JIYEON HWANG <ghkdwldus0807@gmail.com>
2025-07-23 20:46:07 +0900

    feat[#281] : 자신이 작성한 큐레이션을 제외한 랜덤 큐레이션을 조회하는 쿼리 추가

--- src/main/java/org/highfive/backend/curation/repository/jpa/CurationRepository.java
+++ src/main/java/org/highfive/backend/curation/repository/jpa/CurationRepository.java
@@ -1,17 +1,19 @@
 package org.highfive.backend.curation.repository.jpa;
 
 import java.util.List;
-import org.highfive.backend.content.dto.response.HomeContentsResponseDto.CurationDto;
 import org.highfive.backend.curation.entity.Curation;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface CurationRepository extends JpaRepository<Curation, Long> {
 
     @Query("""
-        SELECT c FROM Curation c
-        JOIN FETCH c.user
-        ORDER BY FUNCTION('RANDOM')
-    """)
-    List<Curation> findRandomCurations();
+            SELECT c FROM Curation c
+            JOIN FETCH c.user
+            WHERE c.user.id <> :userId
+            ORDER BY FUNCTION('RANDOM')
+            """)
+    List<Curation> findRandomCurationsExcludeUser(@Param("userId") Long userId, Pageable pageable);
 }
