5c52a76e37120206d6f30be81fa20f66fc748c79
likerhythm <wjdalsdl0412@gmail.com>
2025-07-23 11:47:40 +0900

    refactor[#265]: 사용되지 않는 메서드 삭제

--- src/main/java/org/highfive/backend/slang/SlangRepository.java
+++ src/main/java/org/highfive/backend/slang/SlangRepository.java
@@ -2,6 +2,4 @@ package org.highfive.backend.slang;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface SlangRepository extends JpaRepository<Slang, Long> {
-    void deleteByWord(String word);        // 단어로 삭제
-    Slang findByWord(String word);           // 단어로 찾기
 }
