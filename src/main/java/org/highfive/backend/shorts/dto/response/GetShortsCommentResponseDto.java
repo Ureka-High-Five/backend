5a36d346c389554384feb6198e13f24b95ed9905
JIYEON HWANG <ghkdwldus0807@gmail.com>
2025-07-23 21:29:24 +0900

    refactor[#286] : 쇼츠 단건 조회 ResponseDto에 commentId 추가

--- src/main/java/org/highfive/backend/shorts/dto/response/GetShortsCommentResponseDto.java
+++ src/main/java/org/highfive/backend/shorts/dto/response/GetShortsCommentResponseDto.java
@@ -1,6 +1,7 @@
 package org.highfive.backend.shorts.dto.response;
 
 public record GetShortsCommentResponseDto(
+        Long commentId,
         String comment,
         String userName,
         String profileUrl,
