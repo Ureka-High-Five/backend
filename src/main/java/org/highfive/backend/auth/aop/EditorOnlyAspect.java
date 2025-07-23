b4756ce61405b2d59f4cdb7c3e359b7f3b26d9bb
sangyunpark99 <psangyun5108@gmail.com>
2025-07-23 13:15:47 +0900

    feat[#270]: Editor권한 확인을 S3Controller에도 부여

--- src/main/java/org/highfive/backend/auth/aop/EditorOnlyAspect.java
+++ src/main/java/org/highfive/backend/auth/aop/EditorOnlyAspect.java
@@ -17,7 +17,7 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class EditorOnlyAspect {
 
-    @Before("@annotation(org.highfive.backend.auth.aop.EditorOnly) && within(org.highfive.backend.curation.controller..*)")
+    @Before("@annotation(org.highfive.backend.auth.aop.EditorOnly)")
     public void isValid() {
         final Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
 
