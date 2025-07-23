e06d5491fb999d4112a0523f924dffbd539d66ff
JIYEON HWANG <ghkdwldus0807@gmail.com>
2025-07-23 20:48:26 +0900

    feat[#281] : 사용자 제외 랜덤 큐레이션 조회 시 PageRequest 적용
    - Pageable 파라미터를 활용해 JPQL에 LIMIT 적용

--- src/main/java/org/highfive/backend/content/service/HomeContentService.java
+++ src/main/java/org/highfive/backend/content/service/HomeContentService.java
@@ -1,6 +1,12 @@
 package org.highfive.backend.content.service;
 
+import static org.highfive.backend.global.util.VectorUtil.convertUserVector;
+
 import jakarta.transaction.Transactional;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.highfive.backend.content.dto.response.HomeContentsResponseDto;
@@ -19,14 +25,8 @@ import org.highfive.backend.user.entity.User;
 import org.highfive.backend.user.entity.preference.PreferMetaInfoRepository;
 import org.highfive.backend.user.repository.jpa.UserRepository;
 import org.highfive.backend.user.repository.redis.UserRedisRepository;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
-
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-import static org.highfive.backend.global.util.VectorUtil.convertUserVector;
 
 @Slf4j
 @Service
@@ -39,14 +39,20 @@ public class HomeContentService {
     private final UserRepository userRepository;
     private final CurationRepository curationRepository;
 
+    private final int PAGE_SIZE = 4;
+
     @Transactional
     public Response<HomeContentsResponseDto> getHomeContents(final User user) {
         final MainRecommendDto mainRecommend = recommendMainContentsByUser(user);
         final List<PersonalRecommendDto> personalRecommends = recommendContentsByUser(user, 4);
         final Map<String, List<GenreContentDto>> genreRecommends = recommendContentsByUserGenre(user, 2);
-        final List<CurationDto> curations = CurationMapper.toCurationDto(curationRepository.findRandomCurations());
 
-        final HomeContentsResponseDto result = new HomeContentsResponseDto(mainRecommend, personalRecommends, genreRecommends, curations);
+        final PageRequest pageRequest = PageRequest.of(0, PAGE_SIZE);
+        final List<CurationDto> curations = CurationMapper.toCurationDto(
+                curationRepository.findRandomCurationsExcludeUser(user.getId(), pageRequest));
+
+        final HomeContentsResponseDto result = new HomeContentsResponseDto(mainRecommend, personalRecommends,
+                genreRecommends, curations);
         return new Response<>(SuccessCode.OK.getCode(), result, null);
     }
 
@@ -54,7 +60,8 @@ public class HomeContentService {
         final List<Content> contentsByUserVector = recommendContentsByVector(user, 1);
         final Content content = contentsByUserVector.getFirst();
         final List<String> genres = getGenres(content);
-        return new MainRecommendDto(content.getId(), content.getPostUrl(), content.getDescription(), genres, content.getTitle());
+        return new MainRecommendDto(content.getId(), content.getPostUrl(), content.getDescription(), genres,
+                content.getTitle());
     }
 
     private List<PersonalRecommendDto> recommendContentsByUser(final User user, final int count) {
@@ -67,7 +74,7 @@ public class HomeContentService {
         final String rawVector = userRedisRepository.getUserVector(user.getId());
         final String userVector = convertUserVector(rawVector);
         userRepository.upsertUserVector(user.getId(), userVector);
-        return  contentRepository.findRecommendedContentsByUser(user.getId(), count);
+        return contentRepository.findRecommendedContentsByUser(user.getId(), count);
     }
 
     private Map<String, List<GenreContentDto>> recommendContentsByUserGenre(final User user, final int count) {
@@ -75,13 +82,16 @@ public class HomeContentService {
         final Map<String, List<GenreContentDto>> result = new HashMap<>();
         for (String genre : preferGenresByUser) {
             List<TopContentsByGenreDto> topContentsByGenre = contentRepository.findTopContentsByGenre(genre, 5);
-            result.put(genre, topContentsByGenre.stream().map(tc -> new GenreContentDto(tc.contentId(), tc.thumbnailUrl())).toList());
+            result.put(genre,
+                    topContentsByGenre.stream().map(tc -> new GenreContentDto(tc.contentId(), tc.thumbnailUrl()))
+                            .toList());
         }
         return result;
     }
 
     private List<String> getGenres(final Content content) {
-        final List<Map<String, Object>> contentGenresByContentIds = contentRepository.findContentGenresByContentIds(List.of(content.getId()));
+        final List<Map<String, Object>> contentGenresByContentIds = contentRepository.findContentGenresByContentIds(
+                List.of(content.getId()));
         final List<String> genres = new ArrayList<>();
         for (Map<String, Object> genreInfo : contentGenresByContentIds) {
             String genreName = (String) genreInfo.get("genreName");
