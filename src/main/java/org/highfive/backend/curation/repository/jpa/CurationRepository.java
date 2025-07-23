19e8e4f8a5edb1210bc8becab59b8c9f403389b6
likerhythm <wjdalsdl0412@gmail.com>
2025-07-23 19:51:28 +0900

    feat[#281]: 랜덤 큐레이션 조회 쿼리

--- src/main/java/org/highfive/backend/curation/repository/jpa/CurationRepository.java
+++ src/main/java/org/highfive/backend/curation/repository/jpa/CurationRepository.java
@@ -1,7 +1,17 @@
 package org.highfive.backend.curation.repository.jpa;
 
+import java.util.List;
+import org.highfive.backend.content.dto.response.HomeContentsResponseDto.CurationDto;
 import org.highfive.backend.curation.entity.Curation;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface CurationRepository extends JpaRepository<Curation, Long> {
+
+    @Query("""
+        SELECT c FROM Curation c
+        JOIN FETCH c.user
+        ORDER BY FUNCTION('RANDOM')
+    """)
+    List<Curation> findRandomCurations();
 }
