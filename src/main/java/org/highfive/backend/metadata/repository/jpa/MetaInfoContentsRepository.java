8fc6a530dbaffdfb91cd5fadb1ae1a43d5d06bac
sangyunpark99 <psangyun5108@gmail.com>
2025-07-24 09:51:11 +0900

    feat[#283]: MetaInfoContents와 연관된 데이터 조회시 발생하는 N + 1 문제를 해결하기 위해서 JOIN FETCH 쿼리를 통해 데이터를 조회

--- src/main/java/org/highfive/backend/metadata/repository/jpa/MetaInfoContentsRepository.java
+++ src/main/java/org/highfive/backend/metadata/repository/jpa/MetaInfoContentsRepository.java
@@ -1,10 +1,11 @@
 package org.highfive.backend.metadata.repository.jpa;
 
-import java.util.List;
 import org.highfive.backend.metadata.entity.MetaInfoContents;
 import org.highfive.backend.metadata.entity.MetaType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface MetaInfoContentsRepository extends JpaRepository<MetaInfoContents, Long> {
 
@@ -12,4 +13,7 @@ public interface MetaInfoContentsRepository extends JpaRepository<MetaInfoConten
 
     @Query("DELETE FROM MetaInfoContents mic WHERE mic.content.id = :contentId AND mic.metaInfo.type = :metaType")
     void deleteAllByContentAndType(Long contentId, MetaType metaType);
+
+    @Query("SELECT mic FROM MetaInfoContents mic JOIN FETCH mic.metaInfo WHERE mic.content.id = :contentId")
+    List<MetaInfoContents> findByContentIdWithMetaInfo(Long contentId);
 }
