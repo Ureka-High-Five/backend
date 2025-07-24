d9cbcea59b422f8d7fef1137c074305a518f33fc
sangyunpark99 <psangyun5108@gmail.com>
2025-07-24 21:28:28 +0900

    feat[#309]: Dto로 바로 변환하기 위해서 nativeQuery 제ㅓㄱ

--- src/main/java/org/highfive/backend/review/repository/jpa/ReviewRepository.java
+++ src/main/java/org/highfive/backend/review/repository/jpa/ReviewRepository.java
@@ -2,6 +2,7 @@ package org.highfive.backend.review.repository.jpa;
 
 import java.util.List;
 import java.util.Optional;
+
 import org.highfive.backend.review.entity.Review;
 import org.highfive.backend.review.repository.querydsl.ReviewQueryRepository;
 import org.highfive.backend.user.dto.response.RatedContentResponseDto;
@@ -11,20 +12,16 @@ import org.springframework.data.jpa.repository.Query;
 public interface ReviewRepository extends JpaRepository<Review, Long>, ReviewQueryRepository {
     Optional<Review> findByUserIdAndContentId(Long userId, Long contentId);
 
-    @Query(value = """
-            SELECT
-                r.id as reviewId,
-                c.thumbnail_url as thumbnailUrl,
-                c.title,
-                r.review_text as reviewText,
-                r.rating
-            FROM reviews r
-            JOIN contents c ON r.content_id = c.id
-            WHERE r.user_id = :userId
-              AND (:cursor IS NULL OR r.id < :cursor)
-            ORDER BY r.id DESC
-            LIMIT :limit
-            """, nativeQuery = true)
+    @Query("""
+        SELECT new org.highfive.backend.user.dto.response.RatedContentResponseDto(
+            r.id, c.thumbnailUrl, c.title, r.reviewText, r.rating
+        )
+        FROM Review r
+        JOIN r.content c
+        WHERE r.user.id = :userId
+         AND (:cursor IS NULL OR r.id < :cursor)
+        ORDER BY r.id DESC
+    """)
     List<RatedContentResponseDto> findByUser(
             Long userId,
             Long cursor,
