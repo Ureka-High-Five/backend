522b24c61b59e625ab758d2c95ca7a49200a0ed5
sangyunpark99 <psangyun5108@gmail.com>
2025-07-24 14:36:00 +0900

    feat[#299]: 메시징 처리가 실패한 후, 행동 로그의 상태를 fail로 업데이트 하는 경우도 예외가 발생하는 경우, 로그 남기기

--- src/main/java/org/highfive/backend/rabbitmq/producer/RecommendationMessageProducer.java
+++ src/main/java/org/highfive/backend/rabbitmq/producer/RecommendationMessageProducer.java
@@ -2,10 +2,16 @@ package org.highfive.backend.rabbitmq.producer;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.highfive.backend.action.ActionLogRepository;
+import org.highfive.backend.action.exception.ActionLogErrorCode;
+import org.highfive.backend.action.log.ActionLog;
+import org.highfive.backend.action.log.ActionLogStatus;
+import org.highfive.backend.global.exception.BusinessException;
 import org.highfive.backend.rabbitmq.dto.UserWeightUpdateMessageDto;
 import org.highfive.backend.rabbitmq.exception.MessageDeliveryException;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.retry.annotation.Backoff;
 import org.springframework.retry.annotation.Recover;
 import org.springframework.retry.annotation.Retryable;
@@ -15,7 +21,12 @@ import org.springframework.stereotype.Component;
 @Component
 @RequiredArgsConstructor
 public class RecommendationMessageProducer {
+
+    private final String MANAGED_ACTION_LOG = "managed_action_log";
+
     private final RabbitTemplate rabbitTemplate;
+    private final ActionLogRepository actionLogRepository;
+    private final MongoTemplate mongoTemplate;
 
     @Value("${queue.name}")
     private String queueName;
@@ -25,7 +36,7 @@ public class RecommendationMessageProducer {
             maxAttempts = 3,
             backoff = @Backoff(delay = 200)
     )
-    public void sendWeightUpdateMessage(UserWeightUpdateMessageDto message) {
+    public void sendWeightUpdateMessage(final UserWeightUpdateMessageDto message) {
         try {
             rabbitTemplate.convertAndSend(queueName, message);
         } catch (Exception e) {
@@ -34,7 +45,17 @@ public class RecommendationMessageProducer {
     }
 
     @Recover
-    public void recover(MessageDeliveryException e, String value) {
-        log.error("메시지 전송 실패, value={}, error={}", value, e.getMessage(), e);
+    public void recover(final MessageDeliveryException e, final UserWeightUpdateMessageDto message) {
+        log.error("메시지 전송 실패, id={}, error={}", message.id(), e.getMessage(), e);
+        if(message.id() != null) {
+            try {
+                final ActionLog actionLog = actionLogRepository.findById(message.id()).orElseThrow(() -> new BusinessException(ActionLogErrorCode.ACTION_LOG_NOT_FOUND));
+                actionLog.updateStatus(ActionLogStatus.FAIL);
+                actionLogRepository.save(actionLog);
+                mongoTemplate.save(actionLog, MANAGED_ACTION_LOG);
+            } catch (Exception ex) {
+                log.error("[actionlog_status_update_failed] ActionLog 상테 FAIL 업데이트 중 예외 발생 : id = {}, error = {}", message.id(), ex.getMessage(), ex);
+            }
+        }
     }
 }
