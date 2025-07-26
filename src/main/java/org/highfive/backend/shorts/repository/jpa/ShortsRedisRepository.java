7215059b8b032ea30b4a6ac8b665e455eeadefd4
likerhythm <wjdalsdl0412@gmail.com>
2025-07-26 18:54:30 +0900

    fix[#322]: startIndex 값 수정

--- src/main/java/org/highfive/backend/shorts/repository/jpa/ShortsRedisRepository.java
+++ src/main/java/org/highfive/backend/shorts/repository/jpa/ShortsRedisRepository.java
@@ -34,7 +34,7 @@ public class ShortsRedisRepository {
         if (cursorId != null) {
             for (int i = 0; i < all.size(); i++) {
                 if (all.get(i).id().equals(cursorId)) {
-                    startIndex = i + 1;
+                    startIndex = i;
                     break;
                 }
             }
