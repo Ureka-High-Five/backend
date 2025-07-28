76a3a21c974537ad2f7fe2ca0c4e6319713b15dd
JIYEON HWANG <ghkdwldus0807@gmail.com>
2025-07-28 11:40:47 +0900

    refactor[#325]: Shorts 엔티티의 running_time을 trailer_time로 변경

--- src/main/java/org/highfive/backend/shorts/entity/Shorts.java
+++ src/main/java/org/highfive/backend/shorts/entity/Shorts.java
@@ -1,12 +1,24 @@
 package org.highfive.backend.shorts.entity;
 
-import jakarta.persistence.*;
-import lombok.*;
-import org.highfive.backend.content.entity.Content;
-import org.highfive.backend.global.entity.BaseEntity;
-
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import java.util.ArrayList;
 import java.util.List;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.highfive.backend.content.entity.Content;
+import org.highfive.backend.global.entity.BaseEntity;
 
 @Entity
 @Getter
@@ -33,7 +45,7 @@ public class Shorts extends BaseEntity {
     private String thumbnailUrl;
 
     @Column(nullable = false)
-    private int runningTime;
+    private int trailerTime;
 
     @Builder.Default
     @OneToMany(mappedBy = "shorts", cascade = CascadeType.ALL, orphanRemoval = true)
