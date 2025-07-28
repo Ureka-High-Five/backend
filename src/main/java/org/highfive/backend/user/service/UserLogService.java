9b30c7f0ae1519a9e2cc09a46ada58fc5377f6ef
JIYEON HWANG <ghkdwldus0807@gmail.com>
2025-07-28 23:02:12 +0900

    fix[#337]: 영속 상태의 User를 가져오도록 수정

--- src/main/java/org/highfive/backend/user/service/UserLogService.java
+++ src/main/java/org/highfive/backend/user/service/UserLogService.java
@@ -1,10 +1,14 @@
 package org.highfive.backend.user.service;
 
+import static org.highfive.backend.user.exception.UserErrorCode.USER_NOT_FOUND_ERROR;
+
 import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.highfive.backend.global.dto.Response;
+import org.highfive.backend.global.exception.BusinessException;
 import org.highfive.backend.user.dto.request.CreateContentWatchLogRequestDto;
 import org.highfive.backend.user.entity.User;
+import org.highfive.backend.user.repository.jpa.UserRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -12,17 +16,22 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class UserLogService {
 
+    private final UserRepository userRepository;
+
     @Transactional
     public Response<Void> updateUserWatchInfo(final CreateContentWatchLogRequestDto request, final User user) {
 
         final long previousViewCount = Optional.ofNullable(user.getViewCount()).orElse(0L);
         final long newViewCount = previousViewCount + 1;
 
-        final long previousAverage = Optional.ofNullable(user.getAverageViewTime()).orElse(0L);
-        final long totalWatchTime = previousAverage * previousViewCount + request.watchTime();
-        final long newAverage = totalWatchTime / newViewCount;
+        final long previousAverageViewTime = Optional.ofNullable(user.getAverageViewTime()).orElse(0L);
+        final long totalWatchTime = previousAverageViewTime * previousViewCount + request.watchTime();
+        final long newAverageViewTime = totalWatchTime / newViewCount;
 
-        user.updateUserWatchInfo(newAverage, newViewCount);
+        User existedUser = userRepository.findById(user.getId()).
+                orElseThrow(() -> new BusinessException(USER_NOT_FOUND_ERROR));
+
+        existedUser.updateUserWatchInfo(newAverageViewTime, newViewCount);
 
         return Response.ok(null);
     }
