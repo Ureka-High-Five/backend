3faf88907bc73453987491c069ee556574ecf0c4
JIYEON HWANG <ghkdwldus0807@gmail.com>
2025-07-28 19:32:48 +0900

    feat[#337]: watch log 생성 시 사용자 시청 기록 업데이트 로직을 호출 하도록 변경

--- src/main/java/org/highfive/backend/user/controller/UserLogController.java
+++ src/main/java/org/highfive/backend/user/controller/UserLogController.java
@@ -7,6 +7,7 @@ import org.highfive.backend.action.ActionLogStamp;
 import org.highfive.backend.global.dto.Response;
 import org.highfive.backend.user.dto.request.CreateContentWatchLogRequestDto;
 import org.highfive.backend.user.entity.User;
+import org.highfive.backend.user.service.UserLogService;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -16,12 +17,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class UserLogController {
 
+    private final UserLogService userLogService;
+
     @ActionLogStamp(Action.WATCH)
     @PostMapping("/content/watch-log")
     public Response<Void> createContentWatchLog(
             @Valid @RequestBody final CreateContentWatchLogRequestDto request,
             @AuthenticationPrincipal User user
     ) {
-        return Response.ok(null);
+        return userLogService.updateUserWatchInfo(request, user);
     }
 }
