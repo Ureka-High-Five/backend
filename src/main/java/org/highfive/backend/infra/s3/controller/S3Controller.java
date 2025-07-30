14b0dcf08d62ffae9d17b134ef79fd180a3b6cd5
sangyunpark99 <psangyun5108@gmail.com>
2025-07-30 14:22:14 +0900

    feat[#351]: S3Controller curation, contents 전용 preSignedUrl 제공해주는 api 구현

--- src/main/java/org/highfive/backend/infra/s3/controller/S3Controller.java
+++ src/main/java/org/highfive/backend/infra/s3/controller/S3Controller.java
@@ -3,10 +3,10 @@ package org.highfive.backend.infra.s3.controller;
 import lombok.RequiredArgsConstructor;
 import org.highfive.backend.auth.aop.EditorOnly;
 import org.highfive.backend.global.dto.Response;
-import org.highfive.backend.infra.s3.dto.PresignedUploadResponse;
+import org.highfive.backend.infra.s3.dto.ContentsPresignedUrlResponseDto;
+import org.highfive.backend.infra.s3.dto.CurationPresignedUrlResponseDto;
 import org.highfive.backend.infra.s3.service.S3Service;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,8 +18,14 @@ public class S3Controller {
     private final S3Service s3Service;
 
     @EditorOnly
-    @GetMapping("/presignedUrl/{type}")
-    public Response<PresignedUploadResponse> getPresignedUrl(@PathVariable final String type) {
-        return s3Service.generatePresignedUrl(type);
+    @GetMapping("/presignedUrl/curation")
+    public Response<CurationPresignedUrlResponseDto> curationPreSignedUrl() {
+        return s3Service.generateCurationPreSignedUrl();
+    }
+
+    @EditorOnly
+    @GetMapping("/presignedUrl/contents")
+    public Response<ContentsPresignedUrlResponseDto> contentsPreSignedUrl() {
+        return s3Service.generateContentsPreSignedUrl();
     }
 }
