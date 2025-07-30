35f160d62855e769d6b21c677ace3d5afcb4702e
sangyunpark99 <psangyun5108@gmail.com>
2025-07-30 20:34:06 +0900

    feat[#360]: 가중치가 높은 장르를 2가지만 가져오도록 쿼리 구현

--- src/main/java/org/highfive/backend/user/repository/mongo/UserWeightRepository.java
+++ src/main/java/org/highfive/backend/user/repository/mongo/UserWeightRepository.java
@@ -1,7 +1,14 @@
 package org.highfive.backend.user.repository.mongo;
 
 import org.highfive.backend.user.entity.preference.MongoUserWeight;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+
+import java.util.List;
 
 public interface UserWeightRepository extends MongoRepository<MongoUserWeight, String> {
+
+    @Query(value = "{ 'user_id': ?0, 'type': 'genre' }", sort = "{ 'weight': -1 }")
+    List<MongoUserWeight> findTop2Genres(Long userId, Pageable pageable);
 }
