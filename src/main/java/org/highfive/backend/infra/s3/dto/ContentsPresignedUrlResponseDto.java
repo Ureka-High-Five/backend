dd8ebf3f96169a6c4ed5a6511cace0d9d3e3cd99
sangyunpark99 <psangyun5108@gmail.com>
2025-07-30 14:20:36 +0900

    feat[#351]: 컨텐츠 전용 preSignedUrlResponseDto 구현

---
+++ src/main/java/org/highfive/backend/infra/s3/dto/ContentsPresignedUrlResponseDto.java
@@ -0,0 +1,11 @@
+package org.highfive.backend.infra.s3.dto;
+
+public record ContentsPresignedUrlResponseDto(
+        String imagePresignedUrl,
+        String imageUrl,
+        String shortsPresignedUrl,
+        String shortsUrl,
+        String videoPresignedUrl,
+        String videoUrl
+) {
+}
