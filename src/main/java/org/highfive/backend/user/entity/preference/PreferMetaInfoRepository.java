24a27efd6303602a2cad74f062e608a529c262e5
sangyunpark99 <psangyun5108@gmail.com>
2025-07-30 20:33:43 +0900

    refactor[#360]: 사용하지 않는 메서드 제거

--- src/main/java/org/highfive/backend/user/entity/preference/PreferMetaInfoRepository.java
+++ src/main/java/org/highfive/backend/user/entity/preference/PreferMetaInfoRepository.java
@@ -1,20 +1,6 @@
 package org.highfive.backend.user.entity.preference;
 
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-
-import java.util.List;
 
 public interface PreferMetaInfoRepository extends JpaRepository<PreferMetaInfo, Long> {
-
-    @Query("""
-        SELECT mi.name
-        FROM PreferMetaInfo pmi
-        JOIN pmi.metaInfo mi
-        WHERE pmi.user.id = :userId
-          AND mi.type = 'GENRE'
-        ORDER BY pmi.weight DESC
-    """)
-    List<String> findPreferGenresByUser(Long userId, Pageable pageable);
 }
