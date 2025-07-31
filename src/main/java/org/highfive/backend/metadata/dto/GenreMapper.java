b2ba72a2b5ac1cea2e46c7656e64dda6d2eaa313
sangyunpark99 <psangyun5108@gmail.com>
2025-07-31 11:05:08 +0900

    feat[#366]: 장르의 영어 이름을 한국어로 변경해주는 매퍼 구현

--- src/main/java/org/highfive/backend/metadata/dto/GenreMapper.java
+++ src/main/java/org/highfive/backend/metadata/dto/GenreMapper.java
@@ -5,6 +5,7 @@ import java.util.Map;
 
 public class GenreMapper {
     public static final Map<String, String> CONVERT_DB_GENRE = new HashMap<>();
+    public static final Map<String,String> CONVERT_HOME_GENRE = new HashMap<>();
 
     static {
         CONVERT_DB_GENRE.put("Horror", "공포");
@@ -26,5 +27,27 @@ public class GenreMapper {
         CONVERT_DB_GENRE.put("Fantasy", "Sci-Fi & Fantasy");
         CONVERT_DB_GENRE.put("War", "War & Politics");
         CONVERT_DB_GENRE.put("Talkshow", "Talk");
+    }
+
+    static {
+        CONVERT_HOME_GENRE.put("Horror", "공포");
+        CONVERT_HOME_GENRE.put("Action", "액션");
+        CONVERT_HOME_GENRE.put("Thriller", "스릴러");
+        CONVERT_HOME_GENRE.put("Mystery", "미스터리");
+        CONVERT_HOME_GENRE.put("Family", "가족");
+        CONVERT_HOME_GENRE.put("Comedy", "코미디");
+        CONVERT_HOME_GENRE.put("Animation", "애니메이션");
+        CONVERT_HOME_GENRE.put("Science Fiction", "SF");
+        CONVERT_HOME_GENRE.put("Documentary", "다큐멘터리");
+        CONVERT_HOME_GENRE.put("Drama", "드라마");
+        CONVERT_HOME_GENRE.put("Romance", "로맨스");
+        CONVERT_HOME_GENRE.put("Crime", "범죄");
+        CONVERT_HOME_GENRE.put("History", "역사");
+        CONVERT_HOME_GENRE.put("Music", "음악");
+        CONVERT_HOME_GENRE.put("Children", "키즈");
+        CONVERT_HOME_GENRE.put("Adventure", "어드벤처");
+        CONVERT_HOME_GENRE.put("Fantasy", "판타지");
+        CONVERT_HOME_GENRE.put("War", "전쟁");
+        CONVERT_HOME_GENRE.put("Talkshow", "토크쇼");
     }
 }
