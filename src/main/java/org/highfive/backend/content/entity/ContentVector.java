a38742370693622db6959e44749e20278bc68c2c
sangyunpark99 <psangyun5108@gmail.com>
2025-08-01 08:47:45 +0900

    feat[#368]: ContentVector 엔티티 구현

---
+++ src/main/java/org/highfive/backend/content/entity/ContentVector.java
@@ -0,0 +1,33 @@
+package org.highfive.backend.content.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+import org.highfive.backend.global.entity.BaseEntity;
+
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ContentVector extends BaseEntity {
+
+    @Id @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+
+    @Column(nullable = false, columnDefinition = "TEXT")
+    private String embedding;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "content_id")
+    private Content content;
+
+    public void updateEmbedding(final String embedding) {
+        this.embedding = embedding;
+    }
+
+    public void updateContent(Content content) {
+        this.content = content;
+        content.updateContentVector(this);
+    }
+}
