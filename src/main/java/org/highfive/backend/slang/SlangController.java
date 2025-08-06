1f7b82738dc4ed60e44bd6653b308b4820460aba
likerhythm <wjdalsdl0412@gmail.com>
2025-08-07 01:29:36 +0900

    feat[#397]: 금칙어 필터링 컨트롤러 구현

---
+++ src/main/java/org/highfive/backend/slang/SlangController.java
@@ -0,0 +1,24 @@
+package org.highfive.backend.slang;
+
+import lombok.RequiredArgsConstructor;
+import org.highfive.backend.global.dto.Response;
+import org.highfive.backend.slang.dto.request.SlangFilterRequestDto;
+import org.highfive.backend.slang.dto.response.SlangFilterResponseDto;
+import org.highfive.backend.slang.service.AhoCorasickSlangFilterService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class SlangController {
+
+    private final AhoCorasickSlangFilterService slangFilterService;
+
+    @PostMapping("/slang/filter")
+    public Response<SlangFilterResponseDto> slangFilter(
+            @RequestBody SlangFilterRequestDto request
+    ) {
+        return Response.ok(new SlangFilterResponseDto(slangFilterService.filteringSlang(request.word())));
+    }
+}
