c896517cf7f40398c82313ff078679f17d475470
JIYEON HWANG <ghkdwldus0807@gmail.com>
2025-08-07 11:36:06 +0900

    refactor[#402]: Click ActionLog AOP에서 request body에서 컨텐츠 id를 추출하도록 변경

--- src/main/java/org/highfive/backend/action/log/strategy/ClickActionLogStrategy.java
+++ src/main/java/org/highfive/backend/action/log/strategy/ClickActionLogStrategy.java
@@ -1,14 +1,14 @@
 package org.highfive.backend.action.log.strategy;
 
+import java.util.UUID;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.reflect.MethodSignature;
 import org.highfive.backend.action.Action;
 import org.highfive.backend.action.log.ActionLog;
 import org.highfive.backend.content.exception.ContentErrorCode;
 import org.highfive.backend.global.exception.BusinessException;
+import org.highfive.backend.user.dto.request.UpdateUserWeightByClickRequestDto;
 import org.springframework.stereotype.Component;
-
-import java.util.UUID;
 
 @Component
 public class ClickActionLogStrategy implements ActionLogStrategy {
@@ -31,11 +31,12 @@ public class ClickActionLogStrategy implements ActionLogStrategy {
         String[] paramNames = signature.getParameterNames();
         Object[] args = joinPoint.getArgs();
 
-        for (int i = 0; i < paramNames.length; i++) {
-            if ("contentId".equals(paramNames[i]) && args[i] instanceof Long) {
-                return (Long) args[i];
+        for (Object arg : args) {
+            if (arg instanceof UpdateUserWeightByClickRequestDto dto) {
+                return dto.contentId();
             }
         }
+
         throw new BusinessException(ContentErrorCode.ID_CASTING_ERROR);
     }
 }
